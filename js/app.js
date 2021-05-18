1edc391d4e18c25a7c07d55ba1dab5eeb1b7cc17
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2021-05-18 16:34:04 +0500

    first commit
    no desc

--- js/app.js
+++ js/app.js
@@ -1,8 +1,54 @@
-function ClickButton(){
-  let  x = document.querySelector('.jumbotron');
-  x.innerHTML = '<p>Lemon aprikot buble </p>';
-  alert(x.outerHTML)
-    for (let i = 0; i < document.body.childNodes.length; i++){
-      console.log(document.body.childNodes[i].textContent)
-    }
+// console.log(document)
+// document = html > *
+// document.body = body > *
+let btn = document.querySelector('.btn');
+let box = document.querySelector('.box');
+console.dir(window)
+console.dir(document)
+console.dir(box)
+function Scroll(){
+  document.querySelector('.box').innerHTML = '<h1>Hello</h1>'
 }
+// btn.onclick = function(){
+//   console.log('Click')
+//     // let box = document.querySelector('.box');
+//     // box.classList.add('hide')
+// }
+function x(){
+  box.classList.remove('jump');
+}
+function myFunc(){
+  document.querySelector('.box').classList.add('jump');
+   
+}
+
+
+  
+
+
+// let myFunc2 = function(){
+//   console.log('Click 2')
+// }
+// btn.addEventListener('click', myFunc);
+// btn.addEventListener('click', myFunc2);
+
+function Show(){
+  document.querySelector('.box').classList.add('show');
+  document.querySelector('.box').classList.remove('hide')
+};
+function Hide(){
+  document.querySelector('.box').classList.add('hide')
+  document.querySelector('.box').classList.remove('show')
+};
+// btn.setAttribute('name', 'Men');
+// btn.setAttribute('id', 'myID');
+// console.log(btn)
+
+
+
+// box.setAttribute('id', 'myBox');
+// console.log(box);
+// box.removeAttribute('id');
+// console.log(box)
+// box.removeChild(box.childNodes[0])
+
