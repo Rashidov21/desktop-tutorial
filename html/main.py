37f2f297775dae0744ec6253bcaff802c28e1635
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2021-08-19 17:35:00 +0500

    new files
    js files

--- html/main.py
+++
@@ -1,4 +0,0 @@
-from tqdm import tqdm
-from time import sleep
-for i in tqdm(range(100),desc='loading...'):
-	sleep(0.1)
---
+++ python-basic-tutorial/loops.py
@@ -0,0 +1,49 @@
+# Loops >> sikl >> Takrorlanish
+# while , for in
+
+# range() >> sonlar diapazoni >> sonlar oraligí
+# for i in "str":
+#    print(i)
+# for i in range(1,6):
+#     print(i)
+# apples = ['golden','semeranka']
+# for apple in apples:
+#     print(apple)
+# for i in "str":
+#     print(i, end="")
+# else:
+#     print("For tugadi !")
+
+# for x in range(2,101,2):
+#     print(x)
+
+# s = "string"
+# print(type(len(s)))
+# print(len(s)) # ketma ketlikni uzunligini aniqlash
+# l = len(s)
+# print(l)
+
+arr = [1,5,6,9,87,2,3]
+
+# for x in arr:
+#     print(x)
+# for x in range(len(arr)):
+#     print('Massivni ichida necha element bosa men shuncha consolega chiqaman')
+# for j in range(7):
+#     print('7 marta ishlayapti')
+
+# 1. ismizi kiriting  , har bir harfini yangi qatordan chiqaring
+
+# 2. yoshiz qadar 1 dan boshlab sonlarni chiqaring, har bir takrorlanishda weight
+# degan oz'garuvchiga 3 dan qo'shib tenglab ketin oxirida weight ni va har bitta
+# son console ga chiqsin
+
+# 3. Maxfiy son berilgan foydalanuvchidan sonni tpishini sorang agar u kirtgan son
+# maxfiy sondan katta bosa katta kichik bosa kichik deb chiqsin
+# agar topsa toptiz degan narsa chiqsin
+
+# a = 10 >> int
+# b = 'str' >> str
+
+# int number = 12
+# let number = 10
