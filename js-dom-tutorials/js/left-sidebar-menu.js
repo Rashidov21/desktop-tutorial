57422bf342bc5b73c2ecdcd568d88b1febd07107
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2021-08-27 17:53:56 +0500

    DOM add new files
    Slider methods

--- js-dom-tutorials/js/left-sidebar-menu.js
+++ js-dom-tutorials/js/left-sidebar-menu.js
@@ -11,3 +11,45 @@ menuBtnClose.onclick = function Close(){
     menuBtnOpen.style.display = "block";
     menuBtnClose.style.display = "none";
 }
+// let slides = ['img/air1.jpg','img/air2.jpg','img/air3.jpg'];
+// let frame = 0;
+// let slide = document.getElementById("slide");
+// const next = ()=>{
+//     if(frame >= slides.length){
+//         frame = 0;
+//     }
+//     slide.src = slides[frame]
+//     frame += 1
+//     console.log(frame)
+// }
+// const prev = ()=>{
+//     if(frame <= 0){
+//         frame = slides.length;
+//     }
+//     frame -= 1
+//     slide.src = slides[frame]
+//     console.log(frame)
+//
+// }
+//
+// function Controls(index){
+//     index = parseInt(index);
+//     slide.src = slides[index]
+// }
+
+
+let slides = ['img/air1.jpg','img/air2.jpg','img/air3.jpg'];
+let frame = 0;
+
+let slide = document.getElementById("slide");
+
+function AutoSlider(){
+    if(frame >= slides.length){
+       frame = 0;
+    }
+    slide.src = slides[frame];
+    frame += 1;
+}
+window.onload = () =>{
+    setInterval(AutoSlider,2000) // 1-param function 2-param interval milliseconds
+}
