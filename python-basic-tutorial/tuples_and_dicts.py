cc35a302c0ba042896d7fde698b5a60f313eef60
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2021-09-09 14:42:33 +0500

    python basic is changed
    new file , work_with_time and dicts codes

--- python-basic-tutorial/tuples_and_dicts.py
+++ python-basic-tutorial/tuples_and_dicts.py
@@ -73,3 +73,75 @@
 # t = tuple("uzbekistan")
 # l = "".join(reversed(list(t)))
 # print(l)
+
+
+# Dictionary >> dict()
+
+# Toplam , ushbu toplam ichidagi maulotlarga kalit hamda qiymat sifatida murojaat
+# qilinadi
+
+# key + value
+# Address Book >> key(abdulloh) > value(+99891234567)
+# d = {} # dict()
+#print(type(d)) # class dict
+
+
+# print(user['name']) # Abdullo
+# d = {
+#     "a":True,
+#     "b":{
+#         "bc":152
+#     },
+#     "c":"string",
+#     "d":123,
+#     "e":None,
+# } #dict  ozini ichida istalgan turdagi qiymatlarni saqlay oladi
+# a = d["b"]["bc"] >> 152
+# user = {
+#     "name":"Abdullo",
+#     "age":15,
+#     "skills":['html','css','js']
+# }
+# for i in user:
+#     print(i) # faqar kalitlarini qaytaradi
+
+# for i in user.items():
+#     print(i) # ('name', 'Abdullo') tuple holatida har bitta item'i
+
+# for i in user.values():
+#     print(i) # faqat qiymatlarni olamiz
+
+# newuser = user.copy() # user dict ni nusxasi
+# print(newuser)
+import pprint
+pp = pprint.PrettyPrinter()
+brands = {
+    'phones':"Xiaomi",
+    'laptops':"Apple",
+    'gadgets':"Huawei"
+}
+print(pp.pprint(brands))
+# brands.update({"smartwatches":"Samsung"}) # dict qabul qilib siz korsatgan dictni yangiledi
+# print(brands) #{'phones': 'Xiaomi', 'laptops': 'Apple', 'gadgets': 'Huawei', 'smartwatches': 'Samsung'}
+# brands['phones'] = 'Nokia' # key boyicha valueni yagilash
+# print(brands)
+
+# print(len(brands)) # 3
+# print(brands['phones'])
+# p = brands.get("phones") # dict dan value olish
+# print(p)
+
+# Methods:
+# 1. items()
+# 2. keys()
+# 3. values()
+# 4. get()
+# 5. pop() >> key oraqali elemen ochirish
+# 6. popitem() tasodify bitta elementni ochradi
+# 7. update() dict ni yangiledi
+# 8. clear() dictni tozalaydi
+# 9. copy() nusxasini olish
+
+# task 1
+# input: user word >> olma
+# output: {"o":1,"l":2,"m":3,"a":4}
---
+++ python-basic-tutorial/work_with_time.py
@@ -0,0 +1,51 @@
+"""
+Pythonda vaqt bilan ishlash
+time moduli
+datetime moduli
+calendar moduli
+timeit moduli
+
+"""
+# Modullarni ozimizni dasturimizga yuklymiz
+import time
+# print(time.time()) 1970 1-yanvardan buyon xozirgi vaqtga qadar necha sekund otgani
+# days = ["dushanba","seshanba","chorshanba","payshanba","juma","shanba","yakshanba"]
+# months = ["","yanvar","fevral","mart","aprel","may","iyun","iyul","avgust","sentabr",
+#           "oktabr","noyabr","dekabr"]
+# t = time.localtime()
+# # print(t)
+# print(f"Bugun \n{days[t[6]]}, {t[2]}-{months[t[1]]},{t[0]}, {t[3]}:{t[4]}:{t[5]},"
+#       f"{t[2]}-{t[1]}-{t[0]} ", )
+# hafta kuni = {days[t[6]]}
+# oy kuni va nomi = {t[2]}-{months[t[1]]}
+# xozirgi soat:minut:second = {t[3]}:{t[4]}:{t[5]}
+# qisqa sana = {t[2]}-{t[1]}-{t[0]}
+
+# strftime
+# Sanani time modulidan formatlash orqali uni biz tushinadgan xolatga olish
+# t = time.strftime("%d.%m.%Y")
+# print(t)#bugungi kun:oy:yil
+# t = time.strftime("%H:%M:%S")
+# print(t)
+#
+# users = {}
+# users["loggedin"] = time.strftime("%d.%m.%Y  %H:%M:%S")
+# print(users) # {'loggedin': '09.09.2021  14:32:20'}
+
+# %y = yil >> 21
+# %Y = yil >> 2021
+# %m = oy >> 01..12
+# %b = Yanvar >> yan
+# %B = Yanvar >> Yanvar
+# %d = kun >> 01...31
+# %j = kun yil boshidan beri >> 001 .... 366
+# %U = hafta raqami yil ichida >> 01...53
+# %H = soat 00 dan 23
+# %I = soat 01 dan 12
+# %M = minut 00 dan 59
+# %S = sekund 00 dan 59 , ayrim hollarda 61
+
+
+# for i in range(11):
+#     time.sleep(1) # 1 sekundga scriptni toxtatish
+#     print(i)
