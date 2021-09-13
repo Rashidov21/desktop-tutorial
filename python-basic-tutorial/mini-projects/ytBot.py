7f69335dbb2eb691e8c955d59d0f0fec01d9bb3d
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2021-09-13 19:24:40 +0500

    python basics
    ytBot.py update

--- python-basic-tutorial/mini-projects/ytBot.py
+++ python-basic-tutorial/mini-projects/ytBot.py
@@ -3,14 +3,18 @@ import requests
 from bs4 import BeautifulSoup
 
 bot = telebot.TeleBot("1133552059:AAGZabDmcIcgpukOOo5ItHvYihEvuIMjiDE")
-
-url = "https://pixabay.com/images/search/sun/"
-page = requests.get(url)
+headers = {"User-Agent":'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'}
+url = "https://coderoad.ru/"
+page = requests.get(url=url, headers=headers, verify=False)
 soup = BeautifulSoup(page.content , 'html.parser')
-images = soup.findAll("img")
-@bot.message_handler(commands=['start'])
-def start_command(message):
-    for i in images:
-        bot.send_message(message.chat.id, i["src"])
+card = soup.find('div', class_="card-body")
+links = card.findAll("a", class_="nav-link")
 
-bot.polling()
+for i in links:
+    print(i.text)
+# @bot.message_handler(commands=['start'])
+# def start_command(message):
+#     for i in images:
+#         bot.send_message(message.chat.id, i["src"])
+#
+# bot.polling()
