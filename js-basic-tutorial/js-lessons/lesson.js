cc1335e44f508ea1066af23edce4f9d13f31ac85
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2021-10-08 18:46:22 +0500

    html css
    lesson 7 codes

--- js-basic-tutorial/js-lessons/lesson.js
+++ js-basic-tutorial/js-lessons/lesson.js
@@ -1,18 +1,89 @@
+// Data Type
+// Data Structure
+
+// let num = 10;
+// num = 20;
+// let nums = [10,20,30,50];
+
+// let
+// const
+
+// const x = 10;
+// x = 20;
+
+// const pi = 3.14;
+// let r = 10;
+// console.log(2 * r * pi)
+
+// let appleInfo = "Apple COM inc";
+// let getUserFullName; // Camel Case >> Javascript
+// let get_user_full_name; // Kebab case >> Python
+// let getuserfullname; //
+
+// Data Type
+
+//1 number 
+// let x = 5.2;
+// console.log(x)
+// console.log(typeof x) // number
+
+// 2 NaN  Not a Number
+// alert(1 * "olma") // NaN
+
+// 3 string
+// let str1 = "Apple";
+// let str2 = 'Apple';
+// let str3 = `Apple`;
+
+// console.log(str1 + str2 + str3) //AppleAppleApple
+
+// let s = "123456"
+// console.log(typeof s) // string
+
+// console.log("Qalesan " + " 'ÿaxshimisan' !")
+
+// let age = 15;
+// let name = "Abdulloh";
+// console.log("Foydalanuvchini ismi :" + name)
+// console.log("Foydalanuvchini yoshi :" + age)
+// console.log(`Foydalanuvchini ismi : ${name}`)
+// console.log(`Foydalanuvchini yoshi : ${age}`)
+// let str = "Samsung\nApple\n\tXiaomi";
+// console.log(str);
+
+// function changeColor() {
+//     let box = document.getElementById("box");
+//     box.style.backgroundColor = "red";
+// }
+
+// 4 boolean
+
+// true , false >> 1,0 >> Ha , Yo'q, 
+
+// let b = true;
+// let b2 = false;
+// console.log(typeof b)
+
+// let n = 0;
+// console.log(Boolean(n))
+// console.log(10 < 5)
 
 
-let msg = document.getElementById("msg");
-let number = document.getElementById("number");
+// 10 < 5 // false
+// 10 > 5 // true
 
-function SendMessage(){
-    number = number.value;
-    msg = msg.value;
-    console.log(number,msg)
-    let url = `https://semysms.net/api/3/sms.php?token=11b5e8fea2df7b0466749418885489f3&device=287993&phone=${number}&msg=${msg}`;
-    fetch(url)
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            console.log(data);
-        });
-}
+// 5 null
+
+// hech qaysi qiymat turiga tegishli emas
+
+// 6 undefined 
+// let x;
+// console.log(x)
+
+// alert("hello world")
+
+// let age = prompt("Yoshingizni kiriting.. ") // userdan input kutadi
+// console.log(age)
+
+// let test = confirm("Python or Javascript ") // true yoki false
+// console.log(test)
