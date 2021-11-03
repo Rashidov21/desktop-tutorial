10f0a9e0a5e8922788353b4b94f4adc3cfd6f393
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2021-11-03 14:57:15 +0500

    js tutorial

---
+++ js-basic-tutorial/js-lessons/cheatsheet.js
@@ -0,0 +1,95 @@
+// Data Type 
+// let value = 10 // number
+// value = "Hello" // str "" , '', `` 
+// value = `2 + 2 = ${2+2}` // 2 + 2 =4
+
+// value = true // or false
+
+// value = null // hech qaysi qiymatga tegishli emas
+// value = undefined // qiymati topilmagan
+
+// value = { a: 10, b: 11 } // object 
+
+
+// Vars 
+// let , var , const 
+// let varName = "test" // Camel Case
+// var var_name = "test2" // Snake Case
+
+
+// Conditions
+// console.log(10 > 5) // true
+// console.log(10 < 5) // false
+// console.log(10 <= 5) // false
+// console.log(10 >= 5) // true
+// console.log(10 == 10) // true
+// console.log(10 === 10) // true
+// console.log(10 !== 10) // false
+
+// if else conditions
+// if (10 > 5) {
+//     alert("is working..")
+// } else if (20 > 21) {
+//     alert("not working...")
+// } else {
+//     alert("baribir")
+// }
+
+// || >> or >> yoki
+// console.log(5 > 3 || 2 < 3) // true
+
+// && >> and >> va 
+// console.log(5 > 3 && 2 > 3) //false
+
+// if (10 > 5 && 5 < 3) {
+//     alert("Ishledimi 1 ?") // bu ishlamedi
+// }
+// if (10 > 5 || 5 < 3) {
+//     alert("Ishledimi 2 ?") // bu ishledi
+// }
+// while(){} // 
+// do{}while()
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(i)
+// }
+
+// function funcName(){}
+// function myFunc(name, age) {
+//     alert(`ism=${name}  yosh=${age}`)
+// }
+// myFunc("John", 20)
+
+// const myFunc = function(name, age) {
+//     alert(`ism=${name}  yosh=${age}`)
+// }
+// myFunc("John", 20)
+
+// const myFunc = (name, age) => {
+//     alert(`ism=${name}  yosh=${age}`)
+// }
+// myFunc("John", 20)
+
+// window.onload = function() {
+//     alert("Page is loaded")
+// }
+
+// Objects
+
+// let obj = {
+//     model: "Nike",
+//     price: 25,
+//     isBlue: false
+// }
+// obj.color = true;
+// alert(obj.price) // 25
+// alert(obj["isBlue"]) // false
+
+// for (items in obj) {
+//     alert(obj[items])
+// }
+
+// Arrays
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let arr2 = [true, null, undefined, "salom", 10]
+// console.log(arr[6]) // index = position >> joylashuvi =  7
