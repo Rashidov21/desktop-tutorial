d325fa4ce861d1e0e844e90f7ad13ce72ae025d1
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2021-11-15 20:55:06 +0500

    js tutorial

--- js-basic-tutorial/js-lessons/cheatsheet.js
+++ js-basic-tutorial/js-lessons/cheatsheet.js
@@ -135,3 +135,148 @@
 //     sum += Number(n[i])
 // }
 // console.log(sum)
+
+// task 1
+// let lorem = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor quae magni, hic nisi, nihil eveniet odit laudantium nemo nulla consequuntur similique voluptate earum, dignissimos iusto mollitia commodi voluptas porro quisquam.";
+// lorem = lorem.split(" ")
+// console.log(lorem) // sozlardan iborat massiv 
+
+// function sliceLorem(words_count) {
+//     return lorem.slice(0, words_count)
+// }
+// console.log(sliceLorem(2)) // boshidan 2 ta soz
+
+// task 2
+// let a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+
+// for (let i = 0; i < a.length; i++) {
+//     if (a[i] > 5) {
+//         console.log(a[i])
+//     }
+// }
+
+// task 3 
+// let a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+// let b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+// for (let i = 0; i < a.length; i++) {
+//     for (let k = 0; k < b.length; k++) {
+//         if (a[i] === b[k]) {
+//             console.log(a[i])
+//         }
+//     }
+// }
+// task 4
+// let user = prompt("Sonlar ...");
+// console.log(user.split(","))
+
+// task5 
+// let a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+// console.log(a[0], a[a.length - 1])
+
+// task 6
+// 1 guruch >> 8 kishi uchun
+// 750 gr gosht
+// 250 gr dumba 
+// 1 sabzi
+
+// task 7
+// cars 25000
+// trucks 50000
+// bikes 10000
+// workers = 50% : 10000 / 5000
+// input: cars=20 , trucks=3, bikes=1
+// output: total = 235000 ,  optional=xxx
+
+// function carWash(cars, trucks, bikes) {
+//     carsTotal = cars * 25000;
+//     trucksTotal = trucks * 50000;
+//     bikesTotal = bikes * 10000;
+
+//     total = carsTotal + trucksTotal + bikesTotal;
+//     workers = total / 2
+//     return `Umumiy summa : ${total} \nIshchilar: ${workers}`
+// }
+// console.log(carWash(12, 2, 3))
+
+// task 8
+// 1 . . . 100
+// agar son 3 ga qoldiqsiz bolinsa sonni orniga consolega "Fizz" 
+// agar son 5 ga qoldiqsiz bolinsa sonni orniga consolega "Buzz"
+
+// task 9
+// diapazondagai sonlarni yigindisini hisoblang
+// masalan : 2,10 >> 2,3,4,5,6,7,8,9 = sum ?
+// const sum = (start, end) => {
+//     let result = 0;
+//     for (let i = start; i < end; i++) {
+//         result += i
+//     }
+//     return result
+// }
+// console.log(sum(30, 100))
+
+
+
+// task 10
+
+// # input:let arr = [2,4,6,7,9]
+// # output: sum max sum and min sum
+// var arr = [2, 4, 6, 7, 9];
+// let maxNums = arr.splice(1, 4)
+// var arr = [2, 4, 6, 7, 9]
+// let minNums = arr.splice(0, 4)
+// console.log(Math.max(2, 4, 6, 7, 9))
+// console.log(Math.min(2, 4, 6, 7, 9))
+
+// let maxSum = 0;
+// for (let i = 0; i < maxNums.length; i++) {
+//     maxSum += maxNums[i]
+// }
+// console.log(maxSum)
+
+// let minSum = 0;
+// for (let i = 0; i < minNums.length; i++) {
+//     minSum += minNums[i]
+// }
+// console.log(minSum)
+
+// task 11
+// user kiritgan qator ichidan raqamlarni ajratib oladigan
+// function yozing
+
+// let user = prompt("Password...");
+// //console.log(user.split("")) // massiv
+// user.split("").forEach((elem) => {
+//     elem = Number(elem)
+//     console.log(elem)
+// })
+// task 12
+// # random belgilar ketma ketligidan user kiritgan miqdorda 
+// unikal kod
+// # xosil qilib uni massivda saqlaydigan getRandomCode()
+//  function ni yozing
+// # maslan user = 3
+// # let passwords = ["dddas54f5","asfddv6c","cncnku5d52"]
+
+// let letters = "Dja@7ng$oan03dPyt1h#oncom^:{m}uni2ty?news,n3e*wmodu4lesand_n5ew_pack7ages_a9nd_more"
+// letters = letters.split("")
+// console.log(letters)
+
+// function getRandomCode(passwordsLength, passwordsCount) {
+//     let passwords = []
+//     for (let i = 0; i < passwordsCount; i++) {
+//         let pass = "";
+//         for (let i = 0; i < passwordsLength; i++) {
+//             let rNum = Math.round(Math.random() * letters.length)
+//             pass += letters[rNum]
+//         }
+//         passwords.push(pass)
+//     }
+
+//     return passwords
+// }
+// console.log(getRandomCode(8, 3))
+
+// task 13
+// # input: assalomu alaykum
+// # ouput: asomlyk
