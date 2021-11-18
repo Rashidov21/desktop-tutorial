0b5c2d1418d5a29321aec2eabe357ac1e02b48cf
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2021-11-18 17:52:13 +0500

    quiz app

--- vue-tutorial/vue-17-00/js/quiz.js
+++ vue-tutorial/vue-17-00/js/quiz.js
@@ -3,46 +3,54 @@ const App = {
         return{
             questions:[
                 {
-                    question: 'Вопрос 1',
+                    question: 'Asal ari qancha umr koradi?',
                     answers: [
-                        'Ответ 1',
-                        'Ответ 2',
-                        'Ответ 3',
+                        '45 kun',
+                        '65 kun',
+                        '90 kun',
                     ],
-                    right: 3, // номер правильного ответа
+                    right: 0, // номер правильного ответа
                 },
                 {
-                    question: 'Вопрос 2',
+                    question: 'Oktyabr oyi necha kun?',
                     answers: [
-                        'Ответ 1',
-                        'Ответ 2',
-                        'Ответ 3',
+                        '30 kun',
+                        '31 kun',
+                        '29 kun',
 
                     ],
                     right: 1, // номер правильного ответа
                 },
                 {
-                    question: 'Вопрос 3',
+                    question: 'Bir yilda necha kun bor ?',
                     answers: [
-                        'Ответ 1',
-                        'Ответ 2',
-                        'Ответ 3',
+                        '360 kun',
+                        '361 kun',
+                        '365 kun',
 
                     ],
-                    right: 1, // номер правильного ответа
+                    right: 2, // номер правильного ответа
                 },
             ],
-            questionIndex:0
+            questionIndex:0,
+            points:0,
         }
     },
     methods:{
+        checkAnswer(question, answerIndex){
+
+            let rightAnswer = question.right;
+            if(answerIndex === rightAnswer){
+                this.points += 10
+            }
+
+        },
         nextQuestion(){
             if(this.questionIndex >= this.questions.length){
                 this.questionIndex = 0;
             }else{
                 this.questionIndex++
             }
-
         }
     }
 }
