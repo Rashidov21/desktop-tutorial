e9ad55103555a81c49306bd38798b219a6913ee3
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2021-11-22 14:36:56 +0500

    js dom tutorial

--- js-basic-tutorial/js-lessons/lesson.js
+++ js-basic-tutorial/js-lessons/lesson.js
@@ -75,7 +75,44 @@
 // console.log(`PI ning qiymati=${pi}`) // `` ichida js ozgaruvchilarini berib ketish mumkin
 // alert(`${20+20}`)// 40
 
+// *******LESSON 2 **********
+// 8-xil qiymat turi [Number, String, Boolean, null, undefined, Object ]
+// Boolean >> true , false : 1,0 >> Ha , yoq
+// let value = false;
+// console.log(typeof value)
 
-for (var i = 0; i < 10; i++) {
-    console.log(i++);
-}
+// null , mavjud emas
+// let n = null; //0
+
+// undefined , Topilmadi , yoki qiymat topilmadi
+// let m;
+// console.log(typeof m)
+// let test = confirm("Oq yaxshimi ?") // boolean qiymatini qabul qiladi
+// console.log(test)
+
+// let height = prompt("Boyizni kiriting ...")
+// console.log(`Siz uchun optimal vazn = ${height - 100}kg`)
+
+// let str = "10"; // string
+// str = Number(str) // to Number
+// console.log(str + 10) // 20
+//     // str = number 10 
+// str = String(str) // "10" + 10 == 1010
+// console.log(str + 10)
+
+// console.log("salom" * 2) // NaN >> Not a Number son emas
+
+// console.log(Boolean(null))
+
+// ***Arifmetika****
+// console.log(10 + 10) // 20 
+// console.log(10 - 5) // 5 
+// console.log(10 * 2) // 20 
+// console.log(10 / 2) // 5 
+// console.log(7 % 3) // 1 
+// console.log(2 ** 2) // 4
+
+// console.log(10 + 2 / (2 * 5))
+// task 1
+// let a = +prompt("Kv tomonini kiriting>>")
+// alert(`Kv perimetri = ${4*a}`)
--- js-dom-tutorials/script.js
+++ js-dom-tutorials/script.js
@@ -26,25 +26,80 @@
 
 
 
-function randomColor() {
-    let colors = ["red", "yellow", "green", "blue", "orange"]
-    let rNum = Math.round(Math.random() * colors.length);
-    let box = document.getElementById("box");
-    box.style.background = `${colors[rNum]}`
+// function randomColor() {
+//     let colors = ["red", "yellow", "green", "blue", "orange"]
+//     let rNum = Math.round(Math.random() * colors.length);
+//     let box = document.getElementById("box");
+//     box.style.background = `${colors[rNum]}`
 
-    document.body.style.background = `${colors[rNum]}`
+//     document.body.style.background = `${colors[rNum]}`
+// }
+
+// function ListBg() {
+//     let colors = ["red", "yellow", "green", "blue", "orange"]
+
+//     let ul = document.getElementById("list");
+//     for (let i = 0; i < ul.childNodes.length; i++) {
+//         let rNum = Math.round(Math.random() * colors.length);
+//         if (i % 2 === 1) {
+//             ul.childNodes[i].style.background = `${colors[rNum]}`
+//         }
+
+//     }
+// }
+// ListBg()
+
+// GET Element
+
+// document.getElementById("id_name") // 1 object
+// document.getElementsByTagName("tag_name") // HTMLCollection >> massiv
+// document.getElementsByClassName("class_name") // HTMLCollection >> massiv
+
+// document.querySelector("selector") // css ".box" || "#app" || "p"
+// document.querySelectorAll() // HTMLCollection >> massiv
+
+// let boxes = document.querySelectorAll(".box"); //HTMLCollection >> massiv
+// for (let i = 0; i < boxes.length; i++) {
+//     console.log(boxes[i])
+// }
+// let box = document.querySelector(".box")
+// console.log(box)
+// const pi = 3.14;
+// let app = document.querySelector("#app")
+// app.innerHTML = `<h2>Value of PI = ${pi}</h2>`
+
+
+// function changeClassName() {
+//     let boxes = document.querySelectorAll(".box")
+//     for (let i = 0; i < boxes.length; i++) {
+//         boxes[i].className = "box red"
+//     }
+// }
+
+// function getOldClassName() {
+//     let boxes = document.querySelectorAll(".box")
+//     for (let i = 0; i < boxes.length; i++) {
+//         boxes[i].className = "box"
+//     }
+// }
+
+// div.className = "block";
+// div.classList.add("test")
+// div.classList.remove("anime")
+// div.classList.toggle("anime")
+
+// function color() {
+//     let div = document.querySelector(".box");
+//     div.classList.add("blue")
+// }
+
+// function border() {
+//     let div = document.querySelector(".box");
+//     div.classList.add("border")
+// }
+
+function animation() {
+    let div = document.querySelector(".box");
+    div.classList.add("anime")
+    div.classList.remove(div.classList[0])
 }
-
-function ListBg() {
-    let colors = ["red", "yellow", "green", "blue", "orange"]
-
-    let ul = document.getElementById("list");
-    for (let i = 0; i < ul.childNodes.length; i++) {
-        let rNum = Math.round(Math.random() * colors.length);
-        if (i % 2 === 1) {
-            ul.childNodes[i].style.background = `${colors[rNum]}`
-        }
-
-    }
-}
-ListBg()
