d0fd71d4637961fbd23ebad1c6f5caaa430c56cf
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2021-12-11 16:39:24 +0500

    js dom tutorial

--- js-dom-tutorials/script.js
+++ js-dom-tutorials/script.js
@@ -1,48 +1,53 @@
-// DOM >> Document Object Module 
-// window , document
-// window >> brauzer oynasini ozida aks ettiradi 
-// document >> html hujjatimizni
-// console.log(window)
-// window.open("https://robocode.uz")
-// document
-// window
-// console.log(document.body)
-// заменим цвет фона на красный,
-// document.body.style.transition = "all 0.3s"
-// console.log()
-// typeof
-// console.dir(div)
-// function randomColor() {
-//     let red = Math.round(Math.random() * 255)
-//     let green = Math.round(Math.random() * 255)
-//     let blue = Math.round(Math.random() * 255)
-//     document.body.style.background = `rgb(${red},${green},${blue})`
+const body = document.body;
+const endTime = new Date('December 31 2020 23:59:59');
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minutesEl = document.getElementById('minutes');
+const secondsEl = document.getElementById('seconds');
 
-// }
-// setInterval(randomColor, 2000)
 
-// let div = document.getElementById() // id boyicha
-// let div = document.getElementsByTagName() // tag nomi boyicha
-// let div = document.getElementsByClassName() // class nomi boyicha
-// let div = document.querySelector() // css selectori  boyicha
-// let div = document.querySelectorAll() // css selectori boyicha hamma teglarni
-// function getOrangeBlock() {
-//     let div = document.getElementById("app")
-//     div.style.width = "200px";
-//     div.style.height = "200px";
-//     div.style.background = "orange";
-// }
-// let li = document.getElementsByTagName("li") // massiv korinishida teglar
-// let li = document.querySelectorAll("li")
-// console.log(li)
-// let div = document.querySelector("#app");
-// div.className = "box"
-// let obj = {
-//     name: "Abdullo",
-//     age: 12,
-//     getInfo() {
-//         console.log(this.name)
-//         console.log(this.age)
-//     }
-// }
-// console.log(obj.age)
+setInterval(updateCountdown, 1000)
+setInterval(createSnowFlake, 50);
+
+
+function updateCountdown() {
+    const startTime = new Date();
+    const diff = endTime - startTime;
+    const days = Math.floor(diff / 1000 / 60 / 60 / 24);
+    const hours = Math.floor(diff / 1000 / 60 / 60) % 24;
+    const minutes = Math.floor(diff / 1000 / 60) % 60;
+    const seconds = Math.floor(diff / 1000) % 60;
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = hours < 10 ? '0' + hours : hours;
+    minutesEl.innerHTML = minutes < 10 ? '0' + minutes : minutes;
+    secondsEl.innerHTML = seconds < 10 ? '0' + seconds : seconds;
+}
+
+function createSnowFlake() {
+    const snow_flake = document.createElement('i');
+    snow_flake.classList.add('fas');
+    snow_flake.classList.add('fa-snowflake');
+    snow_flake.style.left = Math.random() * window.innerWidth + 'px';
+    snow_flake.style.animationDuration = Math.random() * 3 + 2 + 's'; // between 2 - 5 seconds
+    snow_flake.style.opacity = Math.random();
+    snow_flake.style.fontSize = Math.random() * 10 + 10 + 'px';
+
+    document.body.appendChild(snow_flake);
+
+    setTimeout(() => {
+        snow_flake.remove();
+    }, 5000)
+}
+
+// SOCIAL PANEL JS
+const floating_btn = document.querySelector('.floating-btn');
+const close_btn = document.querySelector('.close-btn');
+const social_panel_container = document.querySelector('.social-panel-container');
+
+floating_btn.addEventListener('click', () => {
+    social_panel_container.classList.toggle('visible')
+});
+
+close_btn.addEventListener('click', () => {
+    social_panel_container.classList.remove('visible')
+});
