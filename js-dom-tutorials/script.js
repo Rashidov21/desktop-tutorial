b6521201c45cc99dd36c3a89776ddf1087c4a72f
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2021-12-14 16:45:31 +0500

    dom lesson

--- js-dom-tutorials/script.js
+++ js-dom-tutorials/script.js
@@ -1,53 +1,13 @@
-const body = document.body;
-const endTime = new Date('December 31 2020 23:59:59');
-const daysEl = document.getElementById('days');
-const hoursEl = document.getElementById('hours');
-const minutesEl = document.getElementById('minutes');
-const secondsEl = document.getElementById('seconds');
 
+function test(){
+    let cube = document.querySelector(".cube");
+    cube.scrollTop += 50;
+    console.log(window.scrollY)
+    // cube.offsetWidth  = '400px'
+    // cube.offsetHeight = '400px'
 
-setInterval(updateCountdown, 1000)
-setInterval(createSnowFlake, 50);
-
-
-function updateCountdown() {
-    const startTime = new Date();
-    const diff = endTime - startTime;
-    const days = Math.floor(diff / 1000 / 60 / 60 / 24);
-    const hours = Math.floor(diff / 1000 / 60 / 60) % 24;
-    const minutes = Math.floor(diff / 1000 / 60) % 60;
-    const seconds = Math.floor(diff / 1000) % 60;
-    daysEl.innerHTML = days;
-    hoursEl.innerHTML = hours < 10 ? '0' + hours : hours;
-    minutesEl.innerHTML = minutes < 10 ? '0' + minutes : minutes;
-    secondsEl.innerHTML = seconds < 10 ? '0' + seconds : seconds;
 }
-
-function createSnowFlake() {
-    const snow_flake = document.createElement('i');
-    snow_flake.classList.add('fas');
-    snow_flake.classList.add('fa-snowflake');
-    snow_flake.style.left = Math.random() * window.innerWidth + 'px';
-    snow_flake.style.animationDuration = Math.random() * 3 + 2 + 's'; // between 2 - 5 seconds
-    snow_flake.style.opacity = Math.random();
-    snow_flake.style.fontSize = Math.random() * 10 + 10 + 'px';
-
-    document.body.appendChild(snow_flake);
-
-    setTimeout(() => {
-        snow_flake.remove();
-    }, 5000)
-}
-
-// SOCIAL PANEL JS
-const floating_btn = document.querySelector('.floating-btn');
-const close_btn = document.querySelector('.close-btn');
-const social_panel_container = document.querySelector('.social-panel-container');
-
-floating_btn.addEventListener('click', () => {
-    social_panel_container.classList.toggle('visible')
-});
-
-close_btn.addEventListener('click', () => {
-    social_panel_container.classList.remove('visible')
-});
+    // elem.scrollTop >> scrolling qiladi tepadan qochiradi
+    // elem.scrollLeft >> scrolling qiladi Chapdan qochiradi
+    // elem.scrollY >> oynani scrolling qiladi tepadan qochiradi
+    // elem.scrollX >> oynani scrolling qiladi chapdan qochiradi
