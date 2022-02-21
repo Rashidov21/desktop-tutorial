19dc22452547c0713db4c0a863e2d35a13bd6407
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-02-21 14:24:38 +0500

    1-lesson

---
+++ js-basic-tutorial/js/lesson.js
@@ -0,0 +1,69 @@
+// DATA TYPES >> 1 ta ozgaruvchidagi malumot x = 1
+// DATA STRUCTURE >> 1 ta ozgaruvchidagi bir nechta  malumot x = [1,2,3,4,5]
+
+// 1 number >> NaN 
+// let n = 1
+// let n2 = -1
+// let n3 = 1.1
+// console.log(typeof n) // number
+
+// 2- string
+// let str = "Javascript"
+// let str2 = 'Javascript'
+// let str3 = `Javascript`
+
+// 3- Boolean
+// let t = true; 1
+// let f = false; 0
+
+// 4- undefined
+// let x;
+// console.log(typeof x) // undefined
+
+// 5 - null
+// console.log(null)
+
+// 6 - object 
+// let obj = {
+//     name: "John"
+//     age: 25
+// }
+
+// alert()
+// let name = prompt("Ismizni kiriting")
+// let age = +prompt("Yosh ?")
+// let yesOrNo = confirm("Are you sure ?")
+// console.log(yesOrNo)
+// console.log(name + " " + age * 2)
+
+// console.log("str" * 5) // NaN >> Not a number
+
+
+// let num = '13';
+// num = Number(num)
+//     // console.log(typeof num) // string
+// console.log(num * 2) // 26
+
+// let n = 1.2
+// let x = parseInt(n) // x = 1
+// let z = parseFloat(x) // z = 1.0
+
+// let x = String(prompt("Number ?"))
+// console.log(x + 2)
+
+// let x = "1"
+// console.log(Boolean(x) + 1) // 2
+
+// ARIFMETIKA
+// +,-,*,/,%,**
+// console.log(5 + 5) // 10
+// console.log(5 - 2) // 3
+// console.log(5 * 2) // 10
+// console.log(9 / 3) // 3
+// console.log(7 % 2) // .5
+// console.log(2 ** 2) // 4
+// console.log(2 ** 3) // 8
+
+// let a = +prompt("A ni kirit")
+// let p = 4 * a
+// console.log("Perimatr = " + p)
