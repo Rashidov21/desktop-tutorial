d8df7e2e4089b908c4a564183229ff235e4b66c3
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-02-23 14:27:42 +0500

    UPDATE LESSON JS

--- js-basic-tutorial/js/lesson.js
+++ js-basic-tutorial/js/lesson.js
@@ -1,3 +1,5 @@
+// lesson - 2
+
 // DATA TYPES >> 1 ta ozgaruvchidagi malumot x = 1
 // DATA STRUCTURE >> 1 ta ozgaruvchidagi bir nechta  malumot x = [1,2,3,4,5]
 
@@ -67,3 +69,55 @@
 // let a = +prompt("A ni kirit")
 // let p = 4 * a
 // console.log("Perimatr = " + p)
+
+// OPERATORS lesson - 3
+// console.log(5 > 2) // true
+// console.log(5 < 2) // false
+// console.log(5 <= 5) // true
+// console.log(5 >= 4) // true
+// let x = 10;
+// console.log(7 == '7') // true
+// console.log(7 != 7) // false
+// console.log(7 === '7') // false
+// console.log(7 !== 7) // false
+
+// ! >> mantiqiy inkor >> not
+// console.log(!true) //not true
+// console.log(!false) //not false
+
+// || >> mantiqiy Yoki >> or
+
+// let x = 0 || '' || 1 || false
+// console.log(x)
+// && >> mantiqiy VA >> and
+// let y = 0 && '' && 1 && false
+// console.log(y)
+// console.log(false || 0 || "t" || null)
+// console.log(true && false && "t" && 10)
+
+// if >>Agar,
+// else if >>Yoki,
+// else >>Aks holda
+
+// if ("t" && 0) {
+//     console.log("IF")
+// } else if (true && 0) {
+//     console.log("ELSE IF 1")
+// } else if (true && 1) {
+//     console.log("ELSE IF 2")
+// } else {
+//     console.log("ELSE")
+// }
+// let a = +prompt("A = ?")
+// if (a % 2 === 0) {
+//     console.log("Juft")
+// } else {
+//     console.log("Toq")
+// }
+// let a = +prompt("A = ?")
+// let b = +prompt("B = ?")
+// if (a > 2 && b <= 3) {
+//     console.log("Rost")
+// } else {
+//     console.log("Yolgon")
+// }
