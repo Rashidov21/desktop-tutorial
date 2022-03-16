ec98d33fccbe381d0acaaea54b893627656abb86
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-03-16 10:26:08 +0500

    update tailwind project

--- tailwindcss-tutorial/tailwind.config.js
+++ tailwindcss-tutorial/tailwind.config.js
@@ -1,7 +1,10 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-        extend: {},
+        extend: {
+            dark: 'dark',
+        },
     },
+
     plugins: [],
 }
