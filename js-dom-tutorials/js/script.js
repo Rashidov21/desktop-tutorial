da5ef6af539e5002223ee3f1592cc2c09c0b1636
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-05-12 11:07:09 +0500

    update js DOM

--- js-dom-tutorials/js/script.js
+++ js-dom-tutorials/js/script.js
@@ -7,4 +7,49 @@
 // DOM - Document Object Model
 
 // document = html >> object >> sifat + method
+// let obj = {
+//     name: "John",
+//     age:12,
+//     friends:["mike", "david"]
+// }
+// getElement >> 1-object , 2-HTMLAllCollection (1-object. 2-Array)
+// getElement >> object
+// getElements >> HTMLAllCollection >> Array
+// console.dir(obj)
+// let box = document.getElementById("box") // ID boyicha topish
+// let box = document.getElementsByClassName("box")// CLASS boyicha topish
+// let box = document.getElementsByTagName("div")// TAG boyicha topish
+// let box = document.querySelector("#box") // object
+// let li = document.querySelectorAll("li") // NodeList >> ARRAY
+// li.forEach((item)=>{
+//     console.log(item.textContent)
+// })
 
+// let blocks = document.querySelectorAll(".block");// NodeList >> ARRAY
+// function setRandomColor(elements){
+//     let colors = ["red", "yellow", "blue", "pink", "orange", "tomato"]
+//     elements.forEach(element => {
+//         let randomNum = Math.round(Math.random()* elements.length)
+//         element.style.backgroundColor = `${colors[randomNum]}`
+//     });
+// }
+// setRandomColor(blocks)
+// let con = document.querySelector('.container')
+// console.dir(con) // object
+// console.log(con.childNodes); //Array Elements + content
+// console.log(con.children);// child Elements
+// console.log(con.parentElement); // main
+
+// let ul = document.querySelector(".texts"); // ul
+// console.log(ul.children) // array >> li
+// ul.children.forEach(element => {
+//     element.style.fontSize = `${Math.round(Math.random() * 25)}px`
+// });
+// for(let i = 0; i < ul.children.length; i++){
+//     ul.children[i].style.fontSize = `${Math.round(Math.random() * 25)}px`
+// }
+
+function openCloseMenu(){
+    let sidebar = document.querySelector(".mobile");
+    sidebar.classList.toggle("active")
+}
