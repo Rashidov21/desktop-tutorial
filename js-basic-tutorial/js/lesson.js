1baf0352708d85fc2a3128cbcfca8a1b05651bcc
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-05-13 10:11:55 +0500

    Update lesson.js

--- js-basic-tutorial/js/lesson.js
+++ js-basic-tutorial/js/lesson.js
@@ -8,3 +8,25 @@
 // 3- cmd deb yozib ENTER ni bosish
 // 4- cmd da node deb yozib keyin fayl nomi va formatini korsatib ENTER ni bosish
 // (node main.js)
+
+
+let quotes = [
+    "Oddiylik ishonchlilik kalitidir",
+    "Dasturchilar biz xohlaganchalik matematik emas.",
+    "Ishlayaptimi? Tegma.", 
+    "Erta optimallashtirish barcha yomonliklarning ildizidir.", 
+    "C assemblerning kuchiga va ... assemblerning qulayligiga ega.",
+     "Siz o'zingiz yozmagan kodga ishonolmaysiz.",
+      "Agar siz C++ ni qiyin deb hisoblasangiz, ingliz tilini o‘rganishga harakat qilib ko'ring.",
+       "Sinovdan o'tmagan dastur ishlaydigan dastur emas.", 
+       "Dangasalik dasturchining asosiy fazilatidir.",
+       "Algoritmni tushunish uchun uni ko'rish kerak.",
+       "Kelajakni bashorat qilishdan ko'ra uni o'ylab topish osonroq.",
+       "Dasturlash odatda misol orqali o'rgatiladi.",
+       "Ba'zi muammolarni hal qilmaslik, balki undan qochish kerak.", 
+       "Aql-idrokdan qanday foydalanish uning darajasidan muhimroqdir.",
+    "640 KB har qanday vazifa uchun etarli bo'lishi kerak."]
+
+for(let i of quotes){
+    console.log(i)
+}
