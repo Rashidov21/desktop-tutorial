e6a089d2eb5638df51ffe25520dbc3079d9d70f3
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-05-14 16:33:02 +0500

    Update lesson.js

--- js-basic-tutorial/js/lesson.js
+++ js-basic-tutorial/js/lesson.js
@@ -6,12 +6,74 @@
 
 // var , let , const 
 // O'zgaruvchi bu kompyuer xotirasidagi katakchalarni biror bir nom sotiga biriktirish
-var x = 10; // ES5  >> EcmaScript 2009
-let y = 15; // ES6 >> EcmaScript 2015
+// var x = 10; // ES5  >> EcmaScript 2009
+// let y = 15; // ES6 >> EcmaScript 2015
 
 
-const z = 3.14 // ES6 >> EcmaScript 2015
+// const z = 3.14 // ES6 >> EcmaScript 2015
 // const - bu konstanta uni qiymati ozgarmaydi 
 
-var a = apple
-console.log(a + " company"); // apple company
+// var a = "apple";
+// var a = "samsung";
+// console.log(a + " company"); // samsung company
+
+// let phone = "Galaxy Z Fold"
+// phone = "Galaxy A52"
+// console.log(phone)
+
+// let x,y,z;
+// x = 10
+// y = 20
+// z = 30
+// console.log(x + y + z) // console.log(10 + 20 + 30) >> 60
+// let name = "John"
+// let age = 15
+// let language = "Javascript"
+// console.log(name + " " + age + " " + language)
+
+// Nomlash qoidalri
+// 1-ingliz tilida 
+// 2- mantiqan to'g'ri 
+// 3-kichik harflar bilan 
+// 4-uzundan uzun emas 
+// 5-qisqa lo'nda 
+
+// let userprofilephotourl; // xato
+// let userProfilePhotoUrl; // shunisi ma'qul Camel Case
+// let user_profile_photo_url; // bu ham yaxshi
+
+// let user1 = "john" // togri , yaxshi
+// let u2ser = "mike" // togri lekin  yaxshi emas 
+// let 2user = "david" // xato
+// let User = "sara" // togri lekin yaxshi emas
+
+// const pi = 3.14
+// pi = 2.14
+// console.log(pi) //xato
+
+// let COLOR = "#fff" // constanta 
+// COLOR = "#000"
+// console.log(COLOR) //#000
+
+// Shu nomlar bilan ozgaruvchi ochib bolmaydi  
+// var , let , const , if , else , catch , try, function, break , while 
+// for, class , return , script , continue
+
+// Nom topish uchun oson usul
+// Bu nima qiymat saqlaydi savoliga javob topish ! 
+// let userAge = 36
+
+// console.log(2 + 2);
+// alert(2 + 2) // brauzerda alert oynasini ochadi
+// prompt() // input oynasini
+// confirm() // tasdiqlash oynasini ochadi
+
+// if(confirm("ok or cancel")){
+//     alert("OK")
+// }else{
+//     alert("CANCEL")
+// }
+
+// let num1 = +prompt("birinchi son")
+// let num2 = +prompt("ikkinchi son")
+// console.log(num1 + num2);
