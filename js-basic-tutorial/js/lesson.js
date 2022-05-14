48ed1a0b7d8a66f1c0c397d1a5ab2a82a5f02d77
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-05-14 15:40:58 +0500

    Update lesson.js

--- js-basic-tutorial/js/lesson.js
+++ js-basic-tutorial/js/lesson.js
@@ -1,32 +1,17 @@
-// node js ni versiyani bilish uchun 
-// node -v yoki node -V >> v14.17
+// Dastur 
+    // 1-ma'lumotlarni qabul qiladi 2,2 
+    // 2- ularni qayta ishlaydi : 2+2
+    // 3- natijani saqlaydi
+    // 4- natijani qaytaradi
 
-// console.log("Hello world")
-
-// 1- fayl turgan papkani ochish
-// 2- fayl yoliga sichqonchani qoyish
-// 3- cmd deb yozib ENTER ni bosish
-// 4- cmd da node deb yozib keyin fayl nomi va formatini korsatib ENTER ni bosish
-// (node main.js)
+// var , let , const 
+// O'zgaruvchi bu kompyuer xotirasidagi katakchalarni biror bir nom sotiga biriktirish
+var x = 10; // ES5  >> EcmaScript 2009
+let y = 15; // ES6 >> EcmaScript 2015
 
 
-let quotes = [
-    "Oddiylik ishonchlilik kalitidir",
-    "Dasturchilar biz xohlaganchalik matematik emas.",
-    "Ishlayaptimi? Tegma.", 
-    "Erta optimallashtirish barcha yomonliklarning ildizidir.", 
-    "C assemblerning kuchiga va ... assemblerning qulayligiga ega.",
-     "Siz o'zingiz yozmagan kodga ishonolmaysiz.",
-      "Agar siz C++ ni qiyin deb hisoblasangiz, ingliz tilini o‘rganishga harakat qilib ko'ring.",
-       "Sinovdan o'tmagan dastur ishlaydigan dastur emas.", 
-       "Dangasalik dasturchining asosiy fazilatidir.",
-       "Algoritmni tushunish uchun uni ko'rish kerak.",
-       "Kelajakni bashorat qilishdan ko'ra uni o'ylab topish osonroq.",
-       "Dasturlash odatda misol orqali o'rgatiladi.",
-       "Ba'zi muammolarni hal qilmaslik, balki undan qochish kerak.", 
-       "Aql-idrokdan qanday foydalanish uning darajasidan muhimroqdir.",
-    "640 KB har qanday vazifa uchun etarli bo'lishi kerak."]
+const z = 3.14 // ES6 >> EcmaScript 2015
+// const - bu konstanta uni qiymati ozgarmaydi 
 
-for(let i of quotes){
-    console.log(i)
-}
+var a = apple
+console.log(a + " company"); // apple company
