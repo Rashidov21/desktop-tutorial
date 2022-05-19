7a070560857601e5d322624788a4968b4a469c40
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-05-19 11:19:59 +0500

    update js dom lesson

--- js-dom-tutorials/js/script.js
+++ js-dom-tutorials/js/script.js
@@ -1,85 +1,18 @@
-// let btn = document.querySelector("button");
-// btn.setAttribute("name","myBtn") // attr biriktirish
-// console.log(btn.hasAttribute("name")) // attr bor yokli bilish true/false
-// console.log(btn.getAttribute("name")) // attr value olish
-// btn.removeAttribute("name") // attr ni ochirish
+let box = document.querySelector("#box");
+// console.log(box.className); // bu 1 nom ostida class lar nomi : String()
+// console.log(box.classList); // bu array korinishida class lar nomlari
 
-
-// let form = document.querySelector("form")
-// // for(let attr of form.attributes){
-// //     console.log(attr.value)
-// // }
-// if(form.hasAttribute("id")){
-//     console.log("bor")
-// }else{
-//     form.setAttribute("id","myForm")
-// }
-
-// Element.style
-// document.body.style.background = 'khaki';
-// let box = document.querySelector(".box")
-// box.className = 'block' // 1 ta class nomi qoshish
-// box.classList = 'box block red' // bir nechta class nomi qoshish
-
-// box.classList.add() // yangi class qoshadi
-// box.classList.remove() // ochiradi
-// box.classList.contains() // tekshiradi true/false
-// box.classList.toggle() // bosa ochiradi bomasa qoshadi
-// for(let name of box.classList){
-//     console.log(name)
-// }
-// let size = box.classList[1]; // is-size-5
-// let lastElem = size.split("")
-// lastElem[lastElem.length - 1] = 4
-// console.log(lastElem.join(""));// is-size-4
-// console.log(size[size.length - 1]);
-
-
-// Dark Theme
-// document.getElementById('light').style.display= 'none'
-// function mode(state ){
-//     let light = document.getElementById('light')
-//     let dark = document.getElementById('dark')
-//     if(state == "dark"){
-//         document.body.style = `
-//         background-color:#232328;
-//         color:white;
-//         `
-//         dark.style.display= 'none'
-//         light.style.display= 'block'
-//     }else if(state == 'light'){
-//         document.body.style = `
-//         background-color:khaki;
-//         color:black;
-//         `
-//         light.style.display= 'none'
-//         dark.style.display= 'block'
-//     }
-// }
-document.getElementById('light').style.display= 'none'
-function modeV2(state){
-    let light = document.getElementById('light')
-    let dark = document.getElementById('dark')
-    if(state == "light"){
-        document.body.className = "light"
-        light.style.display= 'none'
-        dark.style.display= 'block'
-    }else if(state == "dark"){
-        document.body.className = "dark"
-        dark.style.display= 'none'
-        light.style.display= 'block'
-        
-    }
-}
-
-function changeCubePosition(position){
-    console.log("work");
-    let row = document.getElementById("con");
-   if(position == 1){
-        row.classList = "row pos-1"
-   }else if(position == 2){
-        row.classList = "row pos-2"
-   }else if(position == 3){
-    row.classList = "row pos-3"
-}
+// box.className = "block"
+// box.classList.add() // class qoshish
+// box.classList.remove() // ochirish
+// box.classList.toggle() // bosa ochiradi bomasa qoshadi 
+// box.classList.contains() // bor yoki yoqligini tekshiradi
+// box.classList.add("border")
+// box.classList.remove("red")
+// box.classList.toggle("box")
+// box.classList.toggle("border")
+// console.log(box.classList.contains("box"));
+// console.log(box.classList.contains("border"));
+function showBorder(){
+    box.classList.toggle("border")
 }
