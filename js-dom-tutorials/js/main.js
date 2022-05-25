96fc48361dfd9fe40cb58c3ff4934ef6f7ab6ecb
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-05-25 17:42:33 +0500

    update js dom lesson

--- js-dom-tutorials/js/main.js
+++ js-dom-tutorials/js/main.js
@@ -1,1 +1,54 @@
+// $(".box"); // class orqqali 
+// $("#main"); // id orqqali
+// $("ul"); // teg orqqali
+// $("ul li"); // ul ni ichidagi barcha li teglari
+// document.ready -  DOM butunlay yuklanib bolgan keyin
+// $(document).ready(function() {
+//     $(".btn-slide").click(function() {
+//         $("#panel").slideToggle("slow");
+//         $(this).toggleClass("active");
+//     });
+// });
+$(document).ready(function() {
+    // $("#button").click(function() {
+    //     $(".panel").show() // display:block;
+    // })
 
+    $("#button").click(function() {
+        // $(this).addClass("test") // class qoshis
+        // $(this).addClass("test2") // class qoshis
+        // $(this).removeClass("test2") // class ochirish
+
+        // $(this).parent(".container").click(function() {
+        //     console.log("container mouse");
+        // })
+        // $(".box").animate({ opacity: "0.1", left: "+=400" }, 1200)
+        //     .animate({ opacity: "0.4", top: "+=160", height: "20", width: "20" }, "slow")
+        //     .animate({ opacity: "1", left: "0", height: "100", width: "100" }, "slow")
+        //     .animate({ top: "0" }, "fast")
+        //     .slideUp()
+        //     .slideDown("slow")
+        return true;
+    });
+
+    $("#tool").click(function() {
+        // $('em').animate({ opacity: "show", top: "-75" }, "slow");
+        // $("#content").append("<h4>Hello</h4>")
+        $(this).width("100px")
+        $(this).height("100px")
+        $(".container").toggleClass("blue-bg")
+    })
+
+    // $("form").submit(function(event) {
+    //     event.preventDefault();
+    //     let budget = parseInt($("#budget").text());
+    //     let value = $("#num").val();
+    //     $("#budget").text(budget - value)
+    // })
+
+    $(".panel").click(function() {
+        $(".panel").find("p").slideUp("fast")
+        $(this).find("p").slideToggle("fast")
+    })
+
+})
