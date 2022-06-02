54ce4ba127ca1d051aa9ea1a210d638c0fcc575f
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-06-02 15:27:07 +0500

    update bootstrap 5

--- js-dom-tutorials/js/script.js
+++ js-dom-tutorials/js/script.js
@@ -1,26 +1,32 @@
-// AOS.init({
-//     duration: 1200,
-// });
-window.sr = ScrollReveal({ reset: true });
-sr.reveal('.grid', {
-    duration: 500,
-    rotate: { x: 100, y: 100, x: 0 }
-});
+let form = document.forms[0] // form teglari
 
+// form.addEventListener("change", function(event) {
+//     let list = document.querySelector("#person_info")
+//     let name = form.elements[0].value
+//     let weight = form.elements[1].value
+//     let height = form.elements[2].value
+//     let age = form.elements[3].value
+//     list.children[0].innerHTML = name
+//     list.children[1].innerHTML = weight
+//     list.children[2].innerHTML = height
+//     list.children[3].innerHTML = age
 
-// window.addEventListener("scroll", (event) => {
-//     // console.log(window.scrollX) // qachonki oynada X oqida scroll bosa uni qiymatini chiqaradi
-//     // console.log(window.scrollY) // qachonki oynada Y oqida scroll bosa uni qiymatini chiqaradi
-//     if (window.scrollY > 0 && window.scrollY < 100) {
-//         document.querySelector(".orange").classList.add("shake")
-//     }
-//     if (window.scrollY > 200 && window.scrollY < 400) {
-//         document.querySelector(".red").classList.add("shake")
-//     }
-//     if (window.scrollY > 600 && window.scrollY < 800) {
-//         document.querySelector(".blue").classList.add("shake")
-//     }
-//     if (window.scrollY > 1000 && window.scrollY < 1200) {
-//         document.querySelector(".green").classList.add("shake")
-//     }
 // })
+
+
+// form.autofocus = true
+// console.log(form.elements) // formani barcha maydonlari = array
+// console.log(form.attributes) // formani barcha attributlari = object
+// console.dir(form) // formani barcha attr + method lari
+
+// form.onchange = (event) => { // qachonki input value ozgargan vaqt
+//     console.log(event)
+// }
+// form.elements[0].onfocus = (event) => { // qachonki inputga focus bogan vaqt
+//     console.log("Focused")
+// }
+// form.elements[0].onblur = (event) => { // qachonki inputga focus yoqolgan vaqt
+//     console.log("Blured")
+// }
+// form.reset() // form maydonlarini tozalash
+// form.submit() //toldirilgan maydondagi narsalarni server ga yuborish
