de715f37f1ee8fcbc2fc50bbed745bda51b16e9b
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-06-07 17:06:07 +0500

    Update lesson.js

--- js-basic-tutorial/js/lesson.js
+++ js-basic-tutorial/js/lesson.js
@@ -11,8 +11,8 @@
 // }
 
 //  ***1- declaration >> elon qilingan function***
-//  2 - function expression >> ifoda function
-// 3 - arrow function >> strelka function
+// 2 - arrow function >> strelka function
+//  3 - function expression >> ifoda function
 // 4 - callback function >> callback , biror bir functionni natija qilib qaytaradgan function
 
 // function funksiyaNomi(params) {
@@ -69,3 +69,130 @@
 // task 2
 // input : 5 , text
 // output: text * 5
+
+// let arrowFunc = (x, y, z) => { return (x + y) * z }
+// console.log(arrowFunc(2, 2, 5)) // 20
+
+// let func = x => { return x ** 2 }
+// console.log(func(2)) // 4
+// Function Expression 
+// let func = function() {
+//     console.log("Hello")
+// }
+// func()
+// let plus = function(x, y) {
+//     console.log(x + y)
+// }
+// plus(10, 20)
+// Anonim Function  
+// function() {
+//     console.log("Hello")
+// }
+// window.addEventListener("click", function() {
+//     console.log("Hello")
+// })
+
+// CallBack function
+
+// function sayHi() {
+//     console.log("Hi !")
+// }
+
+// function sayBye() {
+//     console.log("Bye Bye !")
+// }
+
+// function meet(ask, func1, func2) {
+//     if (ask) {
+//         return func1()
+//     } else {
+//         return func2()
+//     }
+// }
+// meet(confirm("Hello ? "), sayHi, sayBye) // callback func
+
+// let age = prompt("Сколько Вам лет?", 18);
+
+// let welcome = (age < 18) ?
+//     () => alert('Привет!') :
+//     () => alert("Здравствуйте!");
+
+// welcome();
+// Ternar operatorlar 
+// let value = 1 > 0 ? 10 : 0 // ? = if , : - else 
+// console.log(value)
+// let age = +prompt("Age of player ?")
+// let playerName = age > 22 ? "Player on" : "Player off"
+// console.log(playerName)
+// false = 0
+// true = 1
+
+// Javascript function
+// 1 - declaration 
+// function funcName(x) {
+//     return x * 2
+// }
+// 2- arrow function
+// let func = x => { return x * 2 }
+// 3 - function expression
+// let func2 = function(x) {
+//         return x * 2
+//     }
+// 4 - callback function
+
+// function call(value, f1, f2) {
+//     if (value) return f1()
+//     else return f2()
+// }
+// call(true, () => { alert("F1 is work") }, function() { alert("F2 is work") })
+
+// task 1
+// input : user dan 5 ta ism qabul qilinsin
+// output : nechtasi "ab" harflari bilan boshlanishini qaytarilsin
+// function checkNames() {
+//     let count = 0
+//     for (let i = 0; i < 5; i++) {
+//         let name = prompt("Name >>> ?")
+//         if (name.toLocaleLowerCase().startsWith("ab")) {
+//             count++
+//         }
+//     }
+//     return count
+// }
+// task 2
+// input: sinfdagi oquvchilar soni va vinetkaga yigiladigan summa(har bir oquvchi uchun)
+// output: umumiy summa
+
+// task 3 
+// input: oquvchilar soni va yoshlari minimum 3 kishi 
+// output : oquvchilarni umumiy yosh darajasi
+
+// function summAge(ages) {
+//     let a = 0;
+//     for (let i = 0; i < ages; i++) {
+//         let age = +prompt("Age ?")
+//         a += age
+//     }
+//     return `Ortacha yosh = ${a / ages}`
+// }
+// console.log(summAge(3))
+
+// function getSummAge(studentCount) {
+//     let summa = 0;
+//     for (let i = 1; i <= studentCount; i++) {
+//         let age = +prompt(`Age of student ${i}`)
+//         summa += age
+//             // i = 0
+//             // i = i + 1 // 1
+//             // i = i * 5 // 
+//     }
+//     return summa
+// }
+// let students = +prompt("Students count ? ")
+// console.log(getSummAge(students) / students)
+// let num = 2;
+// for (let i = 0; i < 3; i++) {
+//     // console.log(num *= 2)
+//     // console.log(num += 2)
+//     // console.log(num -= 2)
+// }
