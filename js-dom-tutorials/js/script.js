ad4725f7d07c6404e14ae495a28b7fd09a816457
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-07-05 16:44:47 +0500

    update dom lesson

--- js-dom-tutorials/js/script.js
+++ js-dom-tutorials/js/script.js
@@ -57,9 +57,77 @@
 // console.log(list.childNodes) // tegni bola elementlarni massiv qaytaradi
 // console.log(list.children) // tegni bola teg elementlarni massiv qaytaradi
 
+// let menu = document.querySelector("#menu");
+// let currentItem = document.querySelector(".current");
+// console.log(menu)
+// console.log(currentItem)
+
+// console.log(currentItem.nextSibling)
+// console.log(currentItem.nextElementSibling.textContent)
+// let tableBlock = document.querySelector(".table");
+
+// let table = document.createElement("table") // <table></table>
+// let tr = document.createElement("tr") // <tr></tr>
+// for (let i = 0; i < 3; i++) {
+//     let td = document.createElement("td") // td
+//     td.innerText = `Item = ${i}` //<td>Item = {i}</td> 
+//     tr.appendChild(td) // <tr><td>Item = {i}</td></tr>
+// }
+// table.appendChild(tr)
+// table.border = "1"
+// tableBlock.appendChild(table)
 
 
+// let img = document.createElement("img")
+// img.src = '../src/image.png'
+// document.body.appendChild(img)
 
+// let title = document.createElement("h1")
+// title.innerText = "Hello world"
+// title.style.color = "#555"
+// document.body.appendChild(title)
 
+let singers = [
+    {
+        name: "Michael Jackson",
+        songs: ["Billi Jean", "I love you"]
+    },
+    {
+        name: "One Direction",
+        songs: ["Kiss you", "Drag me down"]
+    },
+    {
+        name: "Sherali Jo'rayev",
+        songs: ["Gulbadan", "O'zbegim"]
+    },
+]
 
+const MY_ACCESS = "2PSLxRT6iUd99QDS13bmIMFfKlM5rxA41h96Zcl8tUs"
+let imagesBlock = document.querySelector(".images")
+imagesBlock.style.display = "flex"
+fetch(`https://api.unsplash.com/photos/?client_id=${MY_ACCESS}`)
+    .then(res => res.json())
+    .then(data => {
 
+        for (item of data) {
+            let link = document.createElement("a")
+            let href = item.links.download
+            link.href = href
+            link.setAttribute("download", "")
+            link.innerText = "download"
+            link.style.cssText = `
+                color:white;
+                padding:10px 30px;
+                margin:10px;
+                border-radius:10px;
+                background-color:#554;
+
+            `
+
+            imagesBlock.appendChild(link)
+        }
+    })
+
+fetch("https://api.unsplash.com/photos/random?client_id=2PSLxRT6iUd99QDS13bmIMFfKlM5rxA41h96Zcl8tUs")
+    .then(res => res.json())
+    .then(data => console.log(data))
