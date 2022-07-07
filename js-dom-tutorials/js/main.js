c039efdbfcf76b929e144c68c93400dd251f89ad
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-07-07 16:49:05 +0500

    update dom project

--- js-dom-tutorials/js/main.js
+++ js-dom-tutorials/js/main.js
@@ -9,9 +9,10 @@ function openClose() {
 function openAccardion() {
     let obj = document.querySelector(".accardion")
     obj.classList.toggle("down")
+
 }
 
-window.onload = function () {
+function equ() {
     let equal = document.querySelector(".eq")
     let col = document.querySelectorAll('.col')
     setInterval(() => {
@@ -22,3 +23,30 @@ window.onload = function () {
         }
     }, 100)
 }
+function play_single_sound() {
+    document.getElementById('audiotag1').play();
+}
+function pause_single_sound() {
+    document.getElementById('audiotag1').pause();
+}
+window.addEventListener("keypress", (event) => {
+
+    if (event.key == "e") {
+        equ()
+    }
+    else if (event.key == "m") {
+        play_single_sound()
+    }
+    else if (event.key == "p") {
+        pause_single_sound()
+    }
+})
+
+let equal = document.querySelector(".eq")
+console.dir(equal)
+// equal.className = "klass nomi1 klass nomi2"
+// equal.classList = ["klass nomi 1", "klass nomi 2"]
+// equal.classList.add("myClass") klass qoshish
+// equal.classList.remove("myClass") klass ochirish
+// equal.classList.contains("myClass") klass bor yoki yoqligini tekshirish
+// equal.classList.toggle("myClass") klass bor bolsa uni ochiradi bolmasa qoshib qoyadi
