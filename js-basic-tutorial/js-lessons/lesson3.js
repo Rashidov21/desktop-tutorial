26c7aa30ef236a6e9692264819c8e12f490cd4ba
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-08-01 15:44:46 +0500

    Update lesson3.js

--- js-basic-tutorial/js-lessons/lesson3.js
+++ js-basic-tutorial/js-lessons/lesson3.js
@@ -1,1 +1,106 @@
 // for and while , do while loops 
+
+// while , for 
+
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// console.log(4)
+// console.log(10)
+
+// for (let i = 1; i <= 10; i++) 
+// {
+//     console.log(i)
+// }
+
+// let x = 0
+// console.log(x++) // 1  increment ( 1 qo'shish)
+// console.log(x--) // 0 decrement ( 1 ayirish)
+
+// for(sanoq bo'shi; shart; qadam){
+//     takrorlanishi kerak bo'lgan kod
+// }
+// for (let i = 7; i > 0; i--) {
+//     console.log(i) // 7 marta ishga tushadi
+// }
+// let summa = 0;
+// for (let i = 0; i < 3; i++) {
+//     let n = +prompt("sonni kiriting")
+//     if (n > 0) {
+//         summa += n
+//         // summa = summa + n
+//     }
+// }
+// console.log(summa)
+
+// i , k ,j 
+// iteration - takrorlanish
+// let counter = 0;
+// for (let k = 0; k < 56; k++) {
+//     if (k % 5 == 0) {
+//         counter += 1
+//     }
+// }
+// console.log(counter)
+
+// for , while + do while 
+// let i = 0
+// while (i < 3) {
+//     i++
+//     console.log(i)
+// }
+// while (true) {
+//     console.log("WHILE")
+//     break // siklni toxtatish
+// }
+// let i = 0;
+// do {
+//     i++
+//     console.log(i)
+// } while (false)
+
+// let x = 10;
+// while (x > 0) {
+//     x--
+//     if (x == 7 || x == 5) {
+//         continue //keyingi takrorlanishga o'tish
+//     }
+//     console.log(x) // 
+//     if (x == 3) {
+//         break // takrorlanishdan chiqish
+//     }
+
+// }
+
+// task 1
+// let k = 5
+// let n = 7
+// for (let i = 0; i < n; i++) {
+//     console.log(k)
+// }
+// task 2
+// let a = 4
+// let b = 9
+// console.log(b - a)
+// for (let i = a; a <= b; a++) {
+//     console.log(a)
+// }
+
+// let a = 7;
+// let b = 24;
+// for (a; a <= b; a++) {
+//     console.log(a)
+// }
+
+// let a = 7;
+// let b = 24;
+// for (a; a <= b; a++) {
+//     if (a % 2 == 0) {
+//         console.log(a)
+//     }
+// }
+// for (let i = 0; i <= 10; i++) {
+//     console.log(i++)
+// }
+
+
