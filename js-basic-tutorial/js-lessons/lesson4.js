fa0735a3c8cd712fa678a6ccb33046b998f6ba67
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-08-05 15:46:55 +0500

    update js lesson

---
+++ js-basic-tutorial/js-lessons/lesson4.js
@@ -0,0 +1,74 @@
+// Math and String methods 
+
+// console.log(Math.PI) // 3.141592653589793
+
+// let n = 14.5
+// console.log(Math.floor(n)) // 14
+// console.log(Math.ceil(n)) // 15
+// console.log(Math.round(n)) // 15
+// console.log(Math.max(1, 2, 3)) // 3
+// console.log(Math.min(1, 2, 3)) // 1
+
+// console.log(Math.round(Math.random() * 10) + 20)
+
+// let num = 12.340;
+// console.log(typeof num.toFixed(1)); // "string"
+// console.log(num.toFixed(1)); // "12.3"
+// let strNum = "12.3"
+// let n = parseInt(strNum) // sonni butun qilish
+// let n2 = parseFloat(strNum) // sonni qoldiqli qilish
+
+// console.log(n) // int number
+// console.log(n2) // float number
+
+// let n = parseInt(prompt())
+// console.log(typeof n)
+
+// let s1 = "javascript"
+// let s2 = 'python'
+// let s3 = `cpp`
+
+// // console.log(s1[0]) // j
+// // console.log(s1[1]) // a
+// // console.log(s1[2]) // v
+// // console.log(s1.length) // 10
+// // console.log(s1.length - 5) // 5
+// // console.log(s1[s1.length - 1]) // t
+
+// for (let i = 0; i < s1.length; i++) {
+//     console.log(s1[i])
+// }
+// let n = "John"
+// let a = 23
+// let d = "23.3.2000"
+// let s = 25
+
+// let str = `
+//      * name = ${n}
+//      * age = ${a} 
+//      * date = ${d}
+//      * salary = ${s * 100}
+// `
+// console.log(str)
+
+// let user = "Mahatma Gandi"
+// console.log(user.toLowerCase())
+// console.log(user.toUpperCase())
+// console.log(user.includes("andi"))
+// console.log(user.indexOf("a"))
+
+// task 1
+// let s = "javascript is better" // >> JaVaScRiPt...
+// let r = ''
+// for (let i = 0; i < s.length; i++) {
+//     if (i % 2 == 0) {
+//         r += s[i].toUpperCase()
+//     } else {
+//         r += s[i].toLowerCase()
+//     }
+// }
+// console.log(r) // JaVaScRiPt iS BeTtEr
+
+// task 2
+// let text = `Asslomu alaykum Vaalaykum assalom`
+// text da nechta a harfi bor ekanini toping 
