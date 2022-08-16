190a513e806b67cd4ce5c0db21028172b3e661d8
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-08-16 16:47:58 +0500

    update python lessons

--- python-basic-tutorial/intro.py
+++ python-basic-tutorial/intro.py
@@ -1,14 +1,83 @@
 
+# import os
+# print(os.path)
+# IDE - Integrated Development Enviroment, dasturlash uchun mos muhit, vs code yoki pycharm
+# Interpretator - kodni sintaksisga tekshiruvchi dastur , agar xatolik bo'lsa uni toxtatadi
+# bo'lmasa uni kompilyatorga uzatadi
+# Compilator - kodni bitlar darajasida kompyuterga tushuntiruvchi tarjimon dastur
 
-w = "alccdvcdcvdddla"
-r = 0
-r_text = ""
-l = []
-for i in w:
-    ow = w[w.index(i):]
-    if i in ow:
-        r = w.count(i)
-        t = f"{i}{r}"
-    l.append(t)
+# print('Hello world')
+# print("Hello world")  # ma'lumotlarni chop etish
 
-print("".join(list(set(l))))
+# PEP8 - pythonda kod yozish uchun ko'rsatmalar to'plami
+
+# name = "john"
+# age = 10
+# _salary = 1200
+# userProfilePhotoUrl = './photos/'
+# user_profile_photo_url = './photos/'
+# # constanta
+# SALARY = 1200
+# SALARY = 1000
+# print(SALARY)  # 1000
+
+# Data Types
+# int ,float, str , bool , None , list , dict , tuple, set , frozenset, range
+# n = 10
+# x = 10.1
+# z = 'str'
+# print(type(n))
+# print(type(x))
+# print(type(z))
+# version = 3.10
+# s = 'python %s' % version  # python 2
+# s = 'python {0}'.format(version)
+# s = f'python{version}'
+# print(s)
+# s = "python"
+# s = """python"""
+# print("akakfbkabfkabfakfbakf \
+#     ldjalsdnjalsdals\
+#         ksnadlndflasndlasndalsdnas \
+#             asndjklasndlasndalda")
+# print(len(s))  # len = length
+
+# print("python \n 3.10")
+# print("python \t 3.10")
+# print("python \a 3.10")
+
+# Arifmetika
+
+# print(2 + 2)
+# print(2 - 1)
+# print(2 * 2)
+# print(20 / 2)
+# print(17 // 2)
+# print(17 % 2)
+# print(2 ** 2)
+
+# Logika
+# print(2 == 2)
+# print(2 != 2)
+# print(2 >= 2)
+# print(2 <= 2)
+# print(2 and 2)  # &&
+# print(2 or 2)  # ||
+# print("s" in "slaom")  # true
+# print('s' not in "salom")  # false
+# javascript is "better"
+# print('javascript is "better"')
+# print("""javascript "is" 'better' """)
+# print("javascript is \"better\" ")
+# n = int(input('number : '))
+# print(type(n))  # <class 'int'>
+# print(n ** 2)
+
+# int() , str() , float()
+# str_num = "12.6"
+# print(float(str_num) - 0.6)  # 12.0
+# print(int("12") + int("12"))
+
+# print(str(12) + 12)
+# print("12" + 12)
+# print("12" * 5)
