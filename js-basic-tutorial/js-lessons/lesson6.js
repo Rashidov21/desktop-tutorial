2c9459b7be04bb478122442779340d49071a43e4
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-08-17 14:38:19 +0500

    add function lessom

---
+++ js-basic-tutorial/js-lessons/lesson6.js
@@ -0,0 +1,28 @@
+// Function - kod bo'lagi , uni kodni istalgan joyida istalgam marta ishga 
+// tushirish mumkin
+
+// Javascript da function lar 4 xil 
+// 1- declaration 
+// 2- expression 
+// 3- arrow 
+// 4 - callback 
+// function plus(num1, num2) {
+//     return num1 + num2
+// }
+
+// console.log(plus(2, 2)) //4
+
+// function main(name, surname) {
+//     console.log(name + " " + surname)
+//     return name + " " + surname
+// }
+// let result = main("John", "Doe")
+// console.log(result)
+
+// let arr = [1, 2, 3, 4, 5]
+// function getKv(n) {
+//     return n ** 2
+// }
+// for (let i = 0; i <= arr.length; i++) {
+//     console.log(getKv(i))
+// }
