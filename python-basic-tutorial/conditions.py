726aea3c677d3fcb0cdab66040df7083d8a5a769
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-08-18 16:00:32 +0500

    update lesson

---
+++ python-basic-tutorial/conditions.py
@@ -0,0 +1,33 @@
+# if elif else
+
+# if - agar
+# elif - agar yoki
+# else - aks holda
+# n = int(input("n= : "))  # 3
+# if n > 0:
+#     print("if OK")
+# elif n > 1:
+#     print("elif OK")
+# else:
+#     print("NOT OK!")
+
+# age = int(input("age = : "))
+# if age > 18 and age < 24:
+#     print("Welcome ")
+# elif age > 10 or age > 5:
+#     print("Access danied")
+# else:
+#     print("Not valid number !")
+
+# if 1 > 0:print("OK")
+# if 1 > 0:
+#     if 1 > 0:
+#         print(True)
+
+# n = 0
+# if n is not None:
+#     print("Ok")
+# else:
+#     print("ERROR")
+
+
---
+++ python-basic-tutorial/loops.py
@@ -0,0 +1,25 @@
+# import time
+# # while and for loops
+# start = time.time()
+# i = 0
+# while True:
+#     i += 1
+#     print(f"OK - {i}")
+#     if i > 10 ** 5:
+#         break
+# end = time.time()
+# print(f"Time is : {start - end}")
+
+
+# i = 0
+# while i < 100:
+#     i += 1
+#     print(i)
+
+# task  1
+# guess number oyinini yozing
+
+# task 2
+# while orqali kamida 5 ta odamni boyini sonlarda qabul qiling
+# ular orasida eng balandi eng past boyli odamdan necha sm baland ekanini
+# hisoblang
