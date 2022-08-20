8bdba8ac1b8892267a472ef584594672a533ab07
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-08-20 16:30:47 +0500

    update lesson

---
+++ python-basic-tutorial/functions.py
@@ -0,0 +1,70 @@
+# def funksiya nomi(argument,argument):
+#     funsiya tanasi
+# print(plus(2, 2)) # NameError
+
+
+# def plus(x, y):
+#     return x + y
+
+
+# p = plus
+# print(p(1, 2))  # 3
+
+
+# task 1
+# foydalanuvchidan 2 ta son qabul qiladigan  va ular orasida kattasini qayta-
+# radigan funksiya yozing
+# task 2
+# son qabul qiladigan va uni kavadratini qaytaradigan funksiya yozin
+# uni for siklida ishlatish orqali 1 dan 10 gacha bolgan sonlarni
+# kvadratlarini console ga chiqaring
+
+# def return_kvadrat(num):
+#     return num ** 2
+
+
+# for i in range(1, 11):
+#     print(return_kvadrat(i))
+
+# a = 10
+
+
+# def main(x):
+#     i = 0  # local variable
+#     print(a * 2)  # 20
+#     print(x)
+#     if True:
+#         print(i)
+
+
+# main(1)
+# print(i * 2)  # NameError: name 'i' is not defined
+
+# def main(x):
+#     global a
+#     a = 10
+#     return x
+
+
+# print(main(1))  # 1
+# print(a + 2)  # 12
+
+# def main(name=None, surname=None, position="worker"):
+#     return f"{name} {surname} {position}"
+
+
+# # TypeError: main() missing 1 required positional argument: 'name'
+# print(main())  # None None worker
+# print(main("john"))  # john None worker
+# print(main("john", "doe", "staff"))  # "john", "doe", "staff"
+# print(main("john", "doe", ))  # "john", "doe", "worker"
+
+# if b is not None:
+#     print(b)
+
+# task 3
+# user haqida ma'lumot yiguvchi  5 ta argument qabul qiladigan funksiya yozing
+
+# task 4
+# user kiritgan sonni qabul qilib 0 dan shu son qadar takrorlanish hosil qiladigan va shu sonlar orasidagi barcha toq sonlarni chiqaradigan
+# funksiya yozing agar son kiritmasa son 50 ga teng
--- python-basic-tutorial/loops.py
+++ python-basic-tutorial/loops.py
@@ -35,3 +35,53 @@
 # task 3
 # foydalanuvchidan toxtovsiz so'zlar qabul qiling agar sozlarda 2 tadan ortiq
 # a harfi ishlatilgan bo'lsa while ni toxtating
+
+# range() - sonlar diapazoni
+# for i in "abc":
+#     print(i)
+
+# for x in [1, 2, 3, 4, 5]:
+#     print(x, end='')
+
+# print("s" in "salom")  # True
+# print("t" in "salom")  # False
+
+# print(range(10))  # range(0, 10) => 0123456789
+# for i in range(10):
+#     print(i)
+
+# start = 1
+# stop = 10
+# step = 2
+
+# print(range(start, stop, step))  # range(1, 10, 2)
+# for x in range(start, stop, step):
+#     print(x, end='')  # 13579
+
+# for k in range(-1, -10, -2):
+#     print(k)
+
+
+# task 1
+# foydalannuvchi kiritgan sonlar orasida barcha sonlarni konsolga chiqaring
+
+# task 2
+# foydalannuvchi kiritgan sonlar orasida barcha sonlarni konsolga chiqaring
+# kiritgan sonlari ham qo'shilib chiqadi ushbu sonlar yigindisi va ularning
+# kvadrati yigindilarini ham hisoblang
+
+# task 3
+# foydalanuvchi kiritgan matn ichidagi barcha "i" harflarini console ga
+# chiqaring  boshqa harflar sonini sanang "i" dan tashqari
+
+# for i range(10):print(i)
+
+# for i in range(30):
+#     if i == 15:
+#         break
+#     elif i == 10:
+#         continue
+#     print(i)
+
+# else:
+#     print("Takrorlanish tugadi ..")
