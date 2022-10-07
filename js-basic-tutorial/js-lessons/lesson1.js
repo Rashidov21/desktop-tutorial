cf17e4098d3350a9ef4031ce28864fc77f65fd86
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-10-07 19:20:14 +0500

    update lesson

--- js-basic-tutorial/js-lessons/lesson1.js
+++ js-basic-tutorial/js-lessons/lesson1.js
@@ -1,126 +1,19 @@
-// var >> Variable = O'zgaruvchi
-// ES6 - let , const
-// Dastur
-// 1-malumot qabul qiladi
-// 2-uni qayta ishlaydi
-// 3-saqlaydi
-// 4-natijani qaytaradi
-// DNS - Domain Name Server
-// 192.1681.0.1 >>> kun.uz
-
-
-// let a;
 // console.log("Hello world")
+// Dastur
+// 1-ma'lumotlarni qabul qiladi
+// 2-ularni qayta ishlaydi
+// 3-kerak bo'lsa saqlaydi
+// 4-natijani qayatardi
 
-// name = "John"
-// let surname = "Doe"
+// const , let , var 
+let myVariable = "Javascript"
+var x = 10
+const pi = 3.14
 
-// console.log(name, surname)
-// var y = 20
-// var y = 30
-// let x = 10
-// let x = 20 // xatolik bo'lmaydi
-// const pi = 3.14
-// pi = 10 //Assignment to constant variable
-// console.log(x)
-// let x, y, z;
+console.log(myVariable)
+console.log(x, pi)
 
-// x = 3
-// y = 4
-// z = 5
-
-
-// console.log(x * y / z)
-
-// let n = 10; let h = 30;
-// console.log(n + h) // 40
-
-// let userProfileImageUrl = "img/user.png" // Camel case
-// let user_profile_image_url = '' // snake case (kebab case)
-// let userprofileimageurl = ''
-// let USERPROFILEIMAGEURL = ''
-
-
-// Data Types + Data Structured
-// Data Type - Malumot turlari
-// Data structured - Malumot tuzilmalari
-// Malumot turlariga - son , qator , boolean qiymatlari
-// Malumot tuzilmalari - huddi shu malumotlarni kompyuter xotirasida qanday
-// usulda saqlashiga aytiladi
-
-// null - 0 yo'q qiymat
-// undefined - topilmagan qiymat
-// Boolean - True False
-// Number - sonlar
-// String - qator .belgilar
-// Object - obyektlar
-
-// BigInt, Infinity
-
-// console.log(null)
-// let x;
-// console.log(x)
-// console.log(true)
-// console.log(false)
-// console.log(1)
-// console.log(-1)
-// console.log(1.3)
-// console.log("olma")
-// console.log('olma')
-// console.log(`olma`)
-// console.log({ a: "apple" })
-
-// let x = true
-// console.log(typeof x) //boolean
-// x = 1
-// console.log(typeof x) //Number
-// x = "1"
-// console.log(typeof x) // String
-// x = undefined
-// console.log(typeof x) // undefined
-// x = {}
-// console.log(typeof x) // object
-
-
-// console.log("1" + 1) // 11
-// console.log("1" * 3) // 3
-// console.log(3 * "f") // NaN - Not a Number
-
-
-// Boolean
-// let x = Boolean(1) // true
-// let y = Boolean(0) // false
-// console.log(x)
-// console.log(y)
-
-// har qandya 0 bo'lmagan son bu - true
-// har qanday bosh bolmagan string bu true
-
-// let a = String(12)
-// let b = Number("10")
-// console.log(a + b) // 1210
-
-// alert() - ogohlanlantiruvchi oyna
-// prompt() - ma'lumot kiritish mumkin bolgan oyna
-// confirm() - tasdiqlash oynasi
-
-// alert("Siz 18 yoshdan kichiksiz !")
-// let x = prompt("Yoshingizni kiriting :")
-// console.log(Number(x) + 5)
-
-// let x = confirm("Siz rozimisiz ? ")
-// console.log(x)
-
-// Arifmetika
-// console.log(10 + 5)
-// console.log(10 - 5)
-// console.log(10 * 2)
-// console.log(10 / 2)
-
-// console.log(2 ** 2)
-// console.log(2 ** 4)
-// console.log(7 % 3)
-
-// task 1
-// let a = +prompt("a = ?")
-// console.log("perimetr = ", a * 4)
+let userprofilephotourl = "./img/ph.png"
+let uppu = "./img/ph.png"
+let userProfilePhotoUrl = "./img/ph.png"
+let user_profile_photo_url = "./img/ph.png"
