5e4107c2f1b9470006227bcd6774638178a8573b
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-10-10 17:43:06 +0500

    add laughs

--- python-basic-tutorial/mini-projects/laugh_bot/bot.py
+++ python-basic-tutorial/mini-projects/laugh_bot/bot.py
@@ -1,7 +1,12 @@
 # pip install -U aiogram
-
+import random
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from config import TOKEN
+
+btn = InlineKeyboardButton('Kuldirish ', callback_data='laugh')
+laugh_btn = InlineKeyboardMarkup().add(btn)
+
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
@@ -9,7 +14,15 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
-    await message.reply("Salom! , Bu bot faqat kuladi !")
+    await message.reply("Salom! , Bu bot faqat kuladi ! ", reply_markup=laugh_btn)
+
+
+@dp.callback_query_handler(lambda c: c.data)
+async def laugh_send(callback_query: types.CallbackQuery):
+
+    await bot.send_voice(callback_query.from_user.id,
+                         voice=open(
+                             f"laughs/{random.randint(1, 6)}.wav", "rb"))
 
 
 @dp.message_handler()
