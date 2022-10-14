8b0ab1390d6b0fa675857cfeddcd02d86fa4ad2d
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-10-14 18:21:31 +0500

    Update lesson3.js

---
+++ js-basic-tutorial/js-lessons/lesson3.js
@@ -0,0 +1,21 @@
+// Arifmetika 
+// Сложение +,
+// Вычитание -,
+// Умножение *,
+// Деление /,
+// Взятие остатка от деления %,
+// Возведение в степень **.
+
+// console.log(2 + 2) // 4
+// console.log(4 - 2) // 2
+// console.log(4 / 2) // 2
+// console.log(2 * 2) // 4
+// console.log(7 % 3) // 1
+// console.log(2 ** 2) // 4
+
+// let num = +prompt() // int number
+// console.log(num ** 2)
+
+// task 1
+let a = +prompt("kv tomoni")
+console.log(`P = ${4 * a}`)
