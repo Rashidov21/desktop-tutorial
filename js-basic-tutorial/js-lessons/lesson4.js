616c63641e4e26ee992cdf94c93602f4263c1b66
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-10-17 18:39:40 +0500

    add new lesson

---
+++ js-basic-tutorial/js-lessons/lesson4.js
@@ -0,0 +1,28 @@
+// Taqqoshlash operatorlari 
+// console.log(5 > 2) // katta
+// console.log(5 < 2) // kichik
+// console.log(5 >= 2) // katta yoki teng 
+// console.log(5 <= 2) // kichik yoki teng 
+// console.log(5 == 2) // teng : = , == , ===
+// console.log(5 != 2) // teng emas 
+// console.log(2 == "2") // teng emas 
+// console.log(2 === "2") // qatiy teng 
+// console.log(2 !== "2") // qatiy teng emas 
+
+
+// console.log(true && 89 && false && "hshhs") // mantiqiy VA  - AND
+// console.log(0 || 0 || "str" || false) // mantiqiy YOKI  - OR
+// console.log(!true) // mantiqiy INKOR - NOT
+
+// if - agar , else if - yoki , else - aks holda 
+
+// if (shart) { harakat } else if (shart) {harakat} else {harakat}
+// let age = +prompt()
+// if (age > 18) {
+//     console.log("Welcome")
+// } else if (age == 18) {
+//     console.log("OK ! Welcome !")
+// }
+// else {
+//     console.log("Access denied")
+// }
