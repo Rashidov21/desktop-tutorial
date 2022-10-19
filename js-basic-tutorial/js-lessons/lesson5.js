ea08a03f45123231195df632516cae0ad0723fec
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-10-19 18:49:33 +0500

    Update lesson5.js

---
+++ js-basic-tutorial/js-lessons/lesson5.js
@@ -0,0 +1,57 @@
+// while , do while , for loops 
+
+
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// console.log(4)
+// ...
+
+// 1 - while - cheksiz tsikl
+
+// while (true) {
+//     console.log(1)
+// }
+
+// let i = 0;
+// while (i < 10) {
+//     i++
+//     // i = i + 1
+//     console.log(i)
+// }
+// let x = 0
+// do {
+//     x = x + 1
+//     console.log(x)
+// } while (x < 10);
+
+// increment = +1 >>> x++
+// decrement = -1  >>> x--
+// let y = 1
+// console.log(++y) // 2
+// console.log(--y) // 1
+// let control = true;
+// while (control) {
+//     let pass = prompt("Parol ?")
+//     if (pass == "1234") {
+//         console.log("welcome!")
+//         control = false
+
+//     }
+// }
+
+// task 1
+// userdan toxtovsiz sonlar qabul qiling va ular orasida 3 ga qoldiqsiz bo'linadigan 
+// son bolsa uni console ga chiqaring aks holda keyingi sonni kiritishini sorang 
+
+// task 2 
+// userdan toxtovsiz meva nomlari kiritishini sorang agar kiritgan nomlari ichida  anor , olma va behi 
+// nomlari bo'lsa aynan shu nomlarni necha marotaba kiritganini hisoblab keting 10 marotaba
+// nom kiritganidan song tsikl toxtasin va u anor , olma behi nomlarini shu 10 marta ichida necha marta
+// kiritganini console ga chiqaring
+// if(num % 3 == 0){}
+
+
+for (let i = 0; i < 10; i++) {
+    console.log(++i)
+}
