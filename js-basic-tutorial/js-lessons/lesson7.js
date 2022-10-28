4ddfbd1fa8ecd31993db90f7c618d97dd8c48a5f
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-10-28 18:49:08 +0500

    Update lesson7.js

--- js-basic-tutorial/js-lessons/lesson7.js
+++ js-basic-tutorial/js-lessons/lesson7.js
@@ -4,19 +4,30 @@
 // 2 - method - harakat , usul
 
 
-let person = {
-    name: "Odam",
-    age: 23,
-    country: "Uzbekistan",
-    weight: 78.5,
-    height: 179.3,
-    talk() {
-        console.log("Lets talk")
-    },
-    walk: function () {
-        console.log("Walk..")
-    }
-}
+// let person = {
+//     name: "Odam",
+//     age: 23,
+//     country: "Uzbekistan",
+//     weight: 78.5,
+//     height: 179.3,
+//     talk() {
+//         console.log("Lets talk")
+//     },
+//     walk: function () {
+//         console.log("Walk..")
+//     }
+// }
+// console.log("name" in person) // true
+// console.log("salaray" in person) // false
 
-console.log(person.name)
-console.log(person.age)
+// object ni barcha qiymatlarini olish
+// for (let key in person) {
+//     console.log(person[key])
+// }
+
+// console.log(person.name) // odam
+// console.log(person.age) // 23
+
+// console.log(person["country"]) // Uzbekistan
+
+// task 1
