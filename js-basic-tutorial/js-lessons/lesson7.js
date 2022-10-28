4a4d7ca25715bd3f5bed52ae2929817ba0c4e024
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-10-28 18:26:53 +0500

    Update lesson7.js

--- js-basic-tutorial/js-lessons/lesson7.js
+++ js-basic-tutorial/js-lessons/lesson7.js
@@ -2,3 +2,21 @@
 
 // 1 - attribute - sifat
 // 2 - method - harakat , usul
+
+
+let person = {
+    name: "Odam",
+    age: 23,
+    country: "Uzbekistan",
+    weight: 78.5,
+    height: 179.3,
+    talk() {
+        console.log("Lets talk")
+    },
+    walk: function () {
+        console.log("Walk..")
+    }
+}
+
+console.log(person.name)
+console.log(person.age)
