1d6e387511b4d28900f30c74707fe1cb432436e8
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-11-18 18:22:07 +0500

    Update lesson11.js

---
+++ js-basic-tutorial/js-lessons/lesson11.js
@@ -0,0 +1,37 @@
+// filter & map 
+// let objs = [1, 2, 3]
+
+// console.log(objs.filter(
+//     function (item, index, arr) {
+//         console.log("item = " + item)
+//         console.log("index = " + index)
+//         console.log("Array = " + arr)
+//     }
+// )
+// )
+// Array.filter(arrow function)
+
+// let arr = [1, 2, 5, 6, 4, 9, 8, 6, 23, 6, 8, 4, 32, 98, 9]
+// filtered = arr.filter(item => item >= 10)
+// console.log(filtered)
+
+// let users = [
+//     { id: 1, name: "Вася" },
+//     { id: 2, name: "Петя" },
+//     { id: 3, name: "Маша" },
+//     { id: 4, name: "Вася" }
+// ];
+
+// console.log(users.filter(element => element.name == 'Вася'))
+
+// let nums = []
+// for (let i = 0; i < 3; i++) {
+//     let n = +prompt("Son kirit")
+//     nums.push(n)
+// }
+// let evens = nums.filter(num => num % 2 === 0)
+// console.log(evens)
+
+// task 1
+// Userdan meva nomlarini qabul qiling, (kamida 3 ta ) ular orasida "A" yoki "B"
+// harflari ishtirok etgan mevalarni filter qilib alohida chiqaring
