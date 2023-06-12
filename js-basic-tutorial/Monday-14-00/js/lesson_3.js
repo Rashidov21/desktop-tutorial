5d51968ca6b7e59ac28b9d072f4532f0ee590fc4
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2023-06-12 14:24:39 +0500

    update lesson

---
+++ js-basic-tutorial/Monday-14-00/js/lesson_3.js
@@ -0,0 +1,49 @@
+// alert('Hello world!')
+// let a = prompt("String qabul qilish")
+// let a = +prompt("String qabul qilish")
+// alert(a)
+
+// let yesno = confirm("Yes or No")
+// console.log(typeof yesno)
+// console.log(yesno)
+
+// Arifmetika 
+
+// console.log(2*2) // 4
+// console.log(4/2) // 1
+// console.log(1+1) // 2
+// console.log(2-1) // 1
+// console.log(7 % 3) // 1
+
+// console.log(2 + 2 - 4 / (2 * 2)) // 3
+
+// console.log(2**2) // kvadrati
+// console.log(2**3) // kubi
+// console.log(2**10) // 1024
+
+// console.log((2 + 3) * 0.5 );
+
+// let count = 0;
+// // increment  
+// console.log(++count)
+// // count = count +1
+// // decrement
+// console.log(--count)
+// // count = count - 1
+
+// Tenglash operatorlari 
+
+// console.log(1 > 0) // true
+// console.log(1 < 0) // false
+// console.log(1 >= 1); // true
+// console.log(1 <= 1); // true
+// console.log(0 <= 1); // true
+
+// let name = '';
+// console.log(1 == 1); // true
+// console.log(1 == 0); // false
+
+// console.log(1 == "1")//true
+
+// console.log(1 === "1") // false
+
