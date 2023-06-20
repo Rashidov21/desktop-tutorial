ccfdbeb9d288cc058ea613927b29e9d1461074e3
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2023-06-20 10:37:01 +0500

    u

---
+++ js-basic-tutorial/Monday-14-00/js/lesson_5.js
@@ -0,0 +1,45 @@
+// For Loop 
+// for , while 
+// for - sanoqli takrorlanish
+// while - cheksiz takrorlanish
+
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// ... 
+// for(condition){action}
+// for(shart){harakat} 
+// for(sanoq boshi ; boolean ifoda; tsikldagi harakat){}
+// for(let i = 0; i < 10; i++){
+//     // console.log(i)
+//     console.log("Hello world")
+// }
+// for(let k =0; k<3;k++){
+//     let num = +prompt()
+//     console.log(num * 2)
+// }
+// i , k , x , j 
+
+// let users = ["john",'kim','mike']
+// for(let user = 0; user < users.length; user++){
+//     console.log(users[user])
+// }
+// task 1
+// let k = 5;
+// let n = 3;
+
+// for(let i = 0; i < n; i++){
+//     console.log(k)
+// }
+
+// task 2
+// let a = +prompt('a')
+// let b = +prompt('b')
+// if(a<b){
+//     let count = 0
+//     for(b;a<=b;b--){
+//         console.log(b)
+//         count++
+//     }
+//     console.log(count)
+// }
