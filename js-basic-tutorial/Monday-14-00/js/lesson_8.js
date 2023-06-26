663a0fbba4f3cfdc1d8eec1254fffcbbbd0e5cb4
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2023-06-26 14:32:33 +0500

    update

---
+++ js-basic-tutorial/Monday-14-00/js/lesson_8.js
@@ -0,0 +1,37 @@
+// function expression 
+
+let p = function(x,y){
+    return x + y 
+}
+
+// console.log(p(2,2)) // 4
+
+// arrow function 
+
+// let data = d => console.log(d)
+
+// console.log(data(10)) 
+
+// let plus = (a,b) => {return a + b}
+
+// console.log(plus(2,2))
+
+// callback function
+
+// function yes(){
+//     console.log("Yes, OK.")
+// }
+
+// function no(){
+//     console.log("No, Wrong.")
+// }
+// // callback function
+// function askMe(question,func1,func2){
+//     if(question){
+//         return func1()
+//     }else{
+//         return func2()
+//     }
+// }
+
+// askMe(confirm("Yes or No ?"), yes,no)
