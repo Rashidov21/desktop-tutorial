5eb4c8cb4cd5461ad9935569316f492891bd100a
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2023-07-04 17:17:29 +0500

    Update lesson_6.js

--- js-basic-tutorial/Tuesday-16-00/js/lesson_6.js
+++ js-basic-tutorial/Tuesday-16-00/js/lesson_6.js
@@ -44,13 +44,20 @@
 // console.log(n) //2
 
 // function expression - ifoda funktsiya
-let textLen = function(text){
-    return text.length;
-}
-// arrow function - strelka funktsiya
-let wordLen = (text) => {return text.split(' ').length}
+// let textLen = function(text){
+//     return text.length;
+// }
+// // arrow function - strelka funktsiya
+// let wordLen = (text) => {return text.split(' ').length}
 
-console.log(textLen("Javascript"))
-console.log(wordLen("Javascript is better pl!"))
+// console.log(textLen("Javascript"))
+// console.log(wordLen("Javascript is better pl!"))
 
+// task 2 
+// Kun, oy va yil - uchta argumentni qabul qiladigan funktsiya yozing. Agar bizning taqvimimizda bunday sana bo'lsa, True, aks holda False qaytarsin
+// 1-31
+// 1-12
+// 1950-2099
 
+// main(23,7,1566) // True
+// main(23,13,1566) // False 
