eddadcac424826e7f00b1254438b3fcf7622fd24
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2023-07-18 17:09:30 +0500

    update lesson

---
+++ js-basic-tutorial/Tuesday-16-00/dom/querySelectors.js
@@ -0,0 +1,49 @@
+// document.body.style.background = "red";
+
+// console.log(document)
+// console.dir(document)
+
+// DOM - document object model 
+
+// let html = {
+//     title:"HTML 5 Web page"
+// }
+// html.title = "HTML 6 WEB page"
+// console.log(html)
+
+// get element 
+// Elementni ID orqali olish 
+// let mainTag = document.getElementById("main-content")
+// mainTag.style.padding = "50px 100px"
+// mainTag.style.border = "solid 5px red"
+// console.log(mainTag)
+
+// Elementni class orqali olish 
+// let container = document.getElementsByClassName("container")
+// console.log(container)
+
+// querySelector , querySelectorAll
+// let ul = document.querySelector(".list")
+// console.log(ul)
+// let main = document.querySelector("#main-content")
+// console.log(main)
+// let btn = document.querySelector("button")
+// console.log(btn)
+
+// let cont = document.querySelectorAll(".container")
+// console.log(cont)
+let liElems = document.querySelectorAll(".list-item")
+// console.log(liElems.length) // 6
+
+function setRandomLiBgColor(){
+    let n = Math.round(Math.random() * liElems.length-1)
+    let red = Math.round(Math.random() * 255);
+    let green = Math.round(Math.random() * 255);
+    let blue = Math.round(Math.random() * 255);
+    liElems[n].style.backgroundColor = `rgb(${red},${green},${blue})`
+}
+
+
+// for(let elem of liElems){
+//     console.log(elem)
+// }
