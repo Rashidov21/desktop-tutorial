47265168ca5c806fb3ff03d8a80c9533812d299d
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2023-09-18 10:08:39 +0500

    update project

--- django-tutorial/store/store/settings.py
+++ django-tutorial/store/store/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'shop',
+    'django_quill',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'store.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
