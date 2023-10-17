f2ffab36d6d7fbc04f406b0fa1348a55e32258eb
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2023-10-17 15:47:18 +0500

    update  project

--- django-tutorial/firstproject/main/urls.py
+++ django-tutorial/firstproject/main/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path 
+from django.urls import path
+
+# my views 
 from .import views
 
 app_name = 'main'
 
 urlpatterns = [
     path('', views.home_page , name='home'),
+    
+    path('get/', views.get_response, name='test_response'),
+    path('404/', views.handler404, name='page_not_found'),
+    path('redirect/', views.user_redirect, name='redirect'),
 ]
--- django-tutorial/firstproject/main/views.py
+++ django-tutorial/firstproject/main/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
-
+# responses 
+from django.http import HttpResponse,HttpResponseRedirect,HttpResponseNotFound
 # Create your views here.
 
 # controller method 
 def home_page(request):
     data = {'page_title':'Hello Python !'}
     return render(request, template_name='index.html',context=data)
+
+def get_response(request):
+    return HttpResponse('<h3>Response</h3>')
+
+
+def handler404(request):
+    return HttpResponseNotFound("Page not found!")
+
+
+def user_redirect(request):
+    return HttpResponseRedirect("/")
