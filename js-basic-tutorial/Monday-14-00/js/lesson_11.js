23995b4e479e99227374c2157bbf379bfc14f150
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2023-11-22 15:22:34 +0500

    Create lesson_11.js

---
+++ js-basic-tutorial/Monday-14-00/js/lesson_11.js
@@ -0,0 +1,52 @@
+// Object 
+
+// attr - sifat 
+// methot - usul , harakat , funksiya
+
+// let obj = new Object();
+// let obj = {}
+
+let obj = {
+    // obyekt sifatlari 
+    name:"John",
+    surname:"Doe",
+    age:23,
+    // obyekt methodlari
+    talk:function(){
+        console.log("Hello !", "My name is", this.name + ' ' + this.surname)
+    }
+}
+// console.log(obj.name); //John
+// console.log(obj["surname"]); // Doe
+// console.log(obj.age); // 23
+// obj.talk() // Hello ! My name is John Doe
+// console.log(obj.date); // undefined
+
+// ochirish
+delete obj.age
+console.log(obj.age); //undefined
+// yozish
+obj["date"] = "23-10-2005"
+console.log(obj.date); //23-10-2005
+
+// takrorlanish 
+console.log("name" in obj) // true
+console.log("price" in obj) // false
+
+for(let key in obj){
+    console.log(obj[key])
+}
+
+// murakkab malumot tuzilmasi 
+let arrObj = [
+    {
+        data:[[1,2,3],[4,5,6],[7,8,9]],
+    },
+    {
+        data:[[1,2,3],[4,5,6],[7,8,9]],
+    },
+    {
+        data:[[1,2,3],[4,5,6],[7,8,9]],
+    },
+]
+// ushbu arrObj da berilgan object va massiv ichidagi barcha sonlar yigindisini topadigan dastur tuzing
