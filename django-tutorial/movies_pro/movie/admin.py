dff7c428f2dc4e4b6dfd8ea057498c8acc4bcd2a
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2023-12-21 14:44:29 +0500

    Слово пацана
    Tayyor database

--- django-tutorial/movies_pro/movie/admin.py
+++ django-tutorial/movies_pro/movie/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
-
+from .models import (
+    Author,
+    Category,
+    Comment,
+    Genre,
+    Movie,
+    Profile,
+    Roles
+)
 # Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name','slug']
+    prepopulated_fields = {'slug':('name',)}
+    
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ['name','slug']
+    prepopulated_fields = {'slug':('name',)}
+    
+    
+    
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['name','slug']
+    prepopulated_fields = {'slug':('name',)}
+    
+    
+admin.site.register(Roles)
+
+class MovieRoleStackedInline(admin.StackedInline):
+    model = Roles
+    # fields = "__all__"
+    
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['title','slug']
+    prepopulated_fields = {'slug':('title',)}
+    inlines = [MovieRoleStackedInline]
+    
+admin.site.register(Comment)
+admin.site.register(Profile)
